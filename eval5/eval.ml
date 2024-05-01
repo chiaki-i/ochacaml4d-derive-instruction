f3b2d2a0cbb1c4fda587a344017e4a996fc712d2
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-05-01 22:23:07 +0900

    eval5: delinearizing continuation

--- eval5/eval.ml
+++ eval5/eval.ml
@@ -24,7 +24,7 @@ let rec run_c5 c v s t m = match (c, s) with
         end
       | Trail (h) -> h v TNil m
     end
-  | (CApp0 (e1, xs, c), VEnv (vs) :: s) -> f5 e1 xs vs (CApp1 (c)) (v :: s) t m
+  (* | (CApp0 (e1, xs, c), VEnv (vs) :: s) -> f5 e1 xs vs (CApp1 (c)) (v :: s) t m
   | (CApp1 (c), v0 :: s) ->
     begin match v0 with
         VFun (f) -> f v c s t m
@@ -33,9 +33,18 @@ let rec run_c5 c v s t m = match (c, s) with
         run_c5 c' v s' (apnd t' (cons (fun v t m -> run_c5 c v s t m) t)) m
       | _ -> failwith (to_string v0
                        ^ " is not a function; it can not be applied.")
-    end
+    end *)
+  | (CApp0 (v1, v2s, c), VEnv (vs) :: s) ->
+    apply5 v v1 v2s c s t m
+  | (CApp1 (e0, xs, v2s, c), VEnv (vs) :: s) ->
+    f5 e0 xs vs (CApp0 (v, v2s, c)) (VEnv (vs) :: s) t m
+    (* スタックに積むのは vs だけで良いのか？ *)
+  | (CAppS0 (cs), VEnv (v2s) :: s) ->
+    runs_c5 cs (v :: v2s) s t m
+  | (CApply (v0, c), VEnv (vs) :: s) ->
+    apply5 v v0 vs c s t m
   | (COp0 (e1, xs, op, c), VEnv (vs) :: s) ->
-    f5 e1 xs vs (COp1 (op, c)) (v :: s) t m 
+    f5 e1 xs vs (COp1 (op, c)) (v :: s) t m
   | (COp1 (op, c), v0 :: s) ->
     begin match (v0, v) with
         (VNum (n0), VNum (n1)) ->
@@ -50,18 +59,40 @@ let rec run_c5 c v s t m = match (c, s) with
       | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
     end
   | _ -> failwith "stack or cont error"
+(* runs_c5: c -> v list -> s -> t -> m -> v *)
+and runs_c5 c v s t m = match (c, s) with
+    (CApp2 (e0, e1, xs, c), VEnv (vs) :: s) ->
+    f5 e1 xs vs (CApp1 (e0, xs, v, c)) (VEnv (vs) :: s) t m
+  | (CAppS1 (first, xs, cs), VEnv (vs) :: s) ->
+    f5 first xs vs (CAppS0 (cs)) (VEnv (v) :: s) t m
+  | _ -> failwith "runs_c4: unexpected continuation or stack"
+(* apply5 : v -> v -> v list -> c -> s -> t -> m -> v *)
+and apply5 v0 v1 v2s c s t m = match v2s with
+    [] -> app5 v0 v1 c s t m
+  | first :: rest ->
+    app5 v0 v1 (CApply (first, c)) (VEnv (rest) :: s) t m
+(* app5 : v -> v -> c -> s -> t -> m -> v *)
+and app5 v0 v1 c s t m = match v0 with
+      VFun (f) -> f v1 c s t m
+    | VContS (c', s', t') -> run_c5 c' v1 s' t' (MCons ((c, s, t), m))
+    | VContC (c', s', t') ->
+      run_c5 c' v1 s' (apnd t' (cons (fun v t m -> run_c5 c v s t m) t)) m
+    | _ -> failwith (to_string v0
+                      ^ " is not a function; it can not be applied.")
 
 (* f5 : e -> string list -> v list -> c -> s -> t -> m -> v *)
 and f5 e xs vs c s t m = match e with
-    Num (n) -> run_c5 c (VNum (n)) s t m 
+    Num (n) -> run_c5 c (VNum (n)) s t m
   | Var (x) -> run_c5 c (List.nth vs (Env.offset x xs)) s t m
   | Op (e0, op, e1) ->
     f5 e0 xs vs (COp0 (e1, xs, op, c)) (VEnv (vs) :: s) t m
   | Fun (x, e) ->
     run_c5 c
       (VFun (fun v c' s' t' m' -> f5 e (x :: xs) (v :: vs) c' s' t' m')) s t m
-  | App (e0, e1) ->
-    f5 e0 xs vs (CApp0 (e1, xs, c)) (VEnv (vs) :: s) t m
+  (* | App (e0, e1) ->
+    f5 e0 xs vs (CApp0 (e1, xs, c)) (VEnv (vs) :: s) t m *)
+  | App (e0, e1, e2s) ->
+    f5s e2s xs vs (CApp2 (e0, e1, xs, c)) (VEnv (vs) :: s) t m
   | Shift (x, e) -> f5 e (x :: xs) (VContS (c, s, t) :: vs) C0 [] TNil m
   | Control (x, e) -> f5 e (x :: xs) (VContC (c, s, t) :: vs) C0 [] TNil m
   | Shift0 (x, e) ->
@@ -77,6 +108,11 @@ and f5 e xs vs c s t m = match e with
       | _ -> failwith "control0 is used without enclosing reset"
     end
   | Reset (e) -> f5 e xs vs C0 [] TNil (MCons ((c, s, t), m))
+(* f5s: e list -> string list -> v list -> s -> t -> m *)
+and f5s es xs vs c s t m = match es with
+    [] -> runs_c5 c [] s t m
+  | first :: rest ->
+    f5s rest xs vs (CAppS1 (first, xs, c)) (VEnv (vs) :: s) t m
 
 (* f : e -> v *)
 let f expr = f5 expr [] [] C0 [] TNil MNil
--- eval5/value.ml
+++ eval5/value.ml
@@ -1,5 +1,5 @@
 open Syntax
-    
+
 (* Delinearized interpreter : eval5 *)
 
 (* Value *)
@@ -9,9 +9,19 @@ type v = VNum of int
        | VContC of c * s * t
        | VEnv of v list
 
-and c = C0
+(* and c = C0
       | CApp0 of e * string list * c
       | CApp1 of c
+      | COp0 of e * string list * op * c
+      | COp1 of op * c *)
+
+and c = C0
+      | CApp0 of v * v list * c
+      | CApp1 of e * string list * v list * c
+      | CApp2 of e * e * string list * c
+      | CAppS0 of c
+      | CAppS1 of e * string list * c
+      | CApply of v * c
       | COp0 of e * string list * op * c
       | COp1 of op * c
 
