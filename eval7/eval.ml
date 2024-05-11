a66bf2a73ce720311a4ca0d801eb969753dd7792
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-05-11 21:57:55 +0900

    WIP: Op right-to-left

--- eval7/eval.ml
+++ eval7/eval.ml
@@ -30,30 +30,30 @@ let apnd t0 t1 = match t0 with
 let rec f7 e xs c s t m = match s with
     VEnv (vs) :: s ->
     begin match e with
-        Num (n) -> c (VNum (n) :: s) t m 
+        Num (n) -> c (VNum (n) :: s) t m
       | Var (x) -> c ((List.nth vs (Env.offset x xs)) :: s) t m
       | Op (e0, op, e1) ->
-        f7 e0 xs (fun s0 t0 m0 ->
-            begin match s0 with
-                v0 :: VEnv (vs) :: s0 ->
-                f7 e1 xs (fun s1 t1 m1 ->
-                    begin match s1 with
-                        v1 :: v0 :: s1 ->
+        f7 e1 xs (fun s1 t1 m1 ->
+            begin match s1 with
+                v1 :: VEnv (vs) :: s1 -> (* vs を出すために、その手前の v1 も pop *)
+                f7 e0 xs (fun s0 t0 m0 ->
+                    begin match s0 with
+                        v0 :: v1 :: s0 -> (* v1 を出すために、その手前の v0 も pop *)
                         begin match (v0, v1) with
                             (VNum (n0), VNum (n1)) ->
                             begin match op with
-                                Plus -> c (VNum (n0 + n1) :: s1) t1 m1
-                              | Minus -> c (VNum (n0 - n1) :: s1) t1 m1
-                              | Times -> c (VNum (n0 * n1) :: s1) t1 m1
+                                Plus -> c (VNum (n0 + n1) :: s0) t0 m0
+                              | Minus -> c (VNum (n0 - n1) :: s0) t0 m0
+                              | Times -> c (VNum (n0 * n1) :: s0) t0 m0
                               | Divide ->
                                 if n1 = 0 then failwith "Division by zero"
-                                else c (VNum (n0 / n1) :: s1) t1 m1
+                                else c (VNum (n0 / n1) :: s0) t0 m0
                             end
                           | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                            ^ " are not numbers")
                         end
-                      | _ -> failwith "stack error"
-                    end) (VEnv (vs) :: v0 :: s0) t0 m0
+                      | _ -> failwith "stack error op1"
+                    end) (VEnv (vs) :: v1 :: s1) t1 m1
               | _ -> failwith "stack error"
             end) (VEnv (vs) :: VEnv (vs) :: s) t m
       | Fun (x, e) ->
@@ -104,6 +104,8 @@ let rec f7 e xs c s t m = match s with
       | Reset (e) -> f7 e xs idc (VEnv (vs) :: []) TNil (MCons ((c, s, t), m))
     end
   | _ -> failwith "stack error"
-           
+(* f7s: e list -> string list -> s -> t -> m *)
+and f7s es xs c s t m = failwith "not implemented"
+
 (* f : e -> v *)
 let f expr = f7 expr [] idc (VEnv ([]) :: []) TNil MNil
