d590f18dc1970071f725dce4ba429c3d0b2c675f
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-08-03 16:04:45 +0900

    eval6: externalize 'ret' function

--- eval6/eval.ml
+++ eval6/eval.ml
@@ -106,13 +106,14 @@ and f6s es xs vs c s t m = match es with
         end
       ) (VEnv (vs) :: s) t m
 
+and ret c v (VEnv (vs_out) :: s) t m = match vs_out with (* expanding CRet (c) *)
+    [] -> c v s t m
+  | first :: rest -> apply6 v first rest c s t m
+
 and f6t e xs vs vs_out c s t m =
-  let ret v (VEnv (vs_out) :: s) t m = match vs_out with (* expanding CRet (c) *)
-      [] -> c v s t m
-    | first :: rest -> apply6 v first rest c s t m in
   match e with
-    Num (n) -> ret (VNum (n)) (VEnv (vs_out) :: s) t m
-  | Var (x) -> ret (List.nth vs (Env.offset x xs)) (VEnv (vs_out) :: s) t m
+    Num (n) -> ret c (VNum (n)) (VEnv (vs_out) :: s) t m
+  | Var (x) -> ret c (List.nth vs (Env.offset x xs)) (VEnv (vs_out) :: s) t m
   | Op (e0, op, e1) ->
     f6 e1 xs vs (fun v1 s1 t1 m1 ->
         begin match s1 with
@@ -123,12 +124,11 @@ and f6t e xs vs vs_out c s t m =
                     begin match (v0, v1) with
                         (VNum (n0), VNum (n1)) ->
                         begin match op with
-                            Plus -> ret (VNum (n0 + n1)) (VEnv (vs_out) :: s0) t0 m0
-                          | Minus -> ret (VNum (n0 - n1)) (VEnv (vs_out) :: s0) t0 m0
-                          | Times -> ret (VNum (n0 * n1)) (VEnv (vs_out) :: s0) t0 m0
+                            Plus -> ret c (VNum (n0 + n1)) (VEnv (vs_out) :: s0) t0 m0
+                          | Minus -> ret c (VNum (n0 - n1)) (VEnv (vs_out) :: s0) t0 m0                          | Times -> ret c (VNum (n0 * n1)) (VEnv (vs_out) :: s0) t0 m0
                           | Divide ->
                             if n1 = 0 then failwith "Division by zero"
-                            else ret (VNum (n0 / n1)) (VEnv (vs_out) :: s0) t0 m0
+                            else ret c (VNum (n0 / n1)) (VEnv (vs_out) :: s0) t0 m0
                         end
                       | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                        ^ " are not numbers")
