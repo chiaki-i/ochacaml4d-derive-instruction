e180bf85b3a7659471ecd941c158478740fe1d85
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-08-15 21:42:25 +0900

    WIP: eval8: c of f8s now receives v instead of v list

--- eval8/eval.ml
+++ eval8/eval.ml
@@ -28,8 +28,8 @@ let apnd t0 t1 = match t0 with
   | Trail (h) -> cons h t1
 
 (* (>>) : i -> i -> i *)
-let (>>) i0 i1 =
-  fun vs c a s t m -> i0 vs (fun v' s' t' m' -> i1 vs c v' s' t' m') a s t m
+let (>>) i0 i1 = fun vs c a s t m ->
+  i0 vs (fun v' s' t' m' -> i1 vs c v' s' t' m') a s t m
 
 (* push : i *)
 let push = fun vs c a s t m -> c a (a :: s) t m
@@ -43,7 +43,7 @@ let access n = fun vs c a s t m -> c (List.nth vs n) s t m
 (* cur : i -> i *)
 let cur i = fun vs c a s t m ->
     let vfun = VFun (fun v vs_out c' s' t' m' ->
-            i (v :: vs) (* vs_out *) c' v s' t' m' (* TODO: vs_out を入れる *)
+            i (v :: vs) (* vs_out *) c' v s' t' m'
         ) in
     c vfun s t m
 
@@ -68,32 +68,51 @@ let operation op = fun vs c v0 s t m -> match s with
 (* pushmark : i *)
 (* (特に f8 において) vs_out が空であるという情報を積む *)
 (* ここで、vs_out のことを指す *)
-let pushmark = fun vs c a s t m ->
-  let vs_out = [] in
-  c (VEnv vs_out) s t m (* vs_out が空であると決め打ちにする *)
+let pushmark = fun vs c a s t m -> c (VEnv ([])) s t m
 
 (* apply8 : v -> v -> v list -> c -> s -> t -> m -> v *)
-let apply8 v0 v1 vs_out c s t m = match v0 with
-    VFun (f) -> f v1 vs_out c s t m
+let apply8 v0 v1 v2s c s t m = match v0 with
+    VFun (f) -> f v1 v2s c s t m
   | VContS (c', s', t') -> c' v1 s' t' (MCons ((c, s, t), m))
   | VContC (c', s', t') ->
     c' v1 s' (apnd t' (cons (fun v t m -> c v s t m) t)) m
   | _ -> failwith (to_string v0
                     ^ " is not a function; it can not be applied.")
 
-(* apply: i *)
-(* Accumulator に v1 が積まれた状態で実行される *)
-let apply = fun vs c v0 s t m ->
-  match s with v1 :: VEnv (v2s) :: s ->
-    apply8 v0 v1 v2s c s t m
+(* apply : i *)
+(* acc に v0 が積まれた状態で実行される *)
+let apply = fun vs c v0 s t m -> match s with
+  v1 :: VEnv (v2s) :: s -> apply8 v0 v1 v2s c s t m
 
-(* appterm : i *)
-let appterm = fun vs c v s t m ->
+(* aux : i *)
+let aux = fun v2s c v1 s t m ->
   match s with VEnv (v2s) :: s ->
-    c (VEnv (v :: v2s)) s t m
-(* ↑ ここで c に直接 v :: v2s を渡せない問題 *)
-(* eval6b では v list を受け取る継続 cs を定義できていたが、
-   >> で compose しようとすると型が合わなくなるっぽい。 *)
+    c (VEnv (v1 :: v2s)) s t m
+
+(* f8 : e -> string list -> i *)
+let rec f8 e xs = match e with
+    Num (n) -> num n
+  | Var (x) -> access (Env.offset x xs)
+  | Op (e0, op, e1) ->
+    f8 e1 xs >> push >> f8 e0 xs >> operation (op)
+  | Fun (x, e) -> cur (f8 e (x :: xs)) (* TODO: vs_out をどのように持たせるか？ *)
+  | App (e0, e1, e2s) ->
+    (* pushmark inserted by f8s *)
+    (f8s e2s xs) >> push >> (f8 e1 xs) >> push >> (f8 e0 xs) >> apply
+  | _ -> failwith "not implemented"
+    (*
+  | Shift (x, e) -> shift (f8 e (x :: xs) vs)
+  | Control (x, e) -> control (f8 e (x :: xs) vs)
+  | Shift0 (x, e) -> shift0 (f8 e (x :: xs) vs)
+  | Control0 (x, e) -> control0 (f8 e (x :: xs) vs)
+  | Reset (e) -> reset (f8 e xs vs)
+ *)
+(* f8s : e -> string list -> env -> i *)
+and f8s es xs = match es with
+    [] -> pushmark
+  | first :: rest ->
+    (f8s rest xs) >> push >> (f8 first xs) >> aux
+
 
 (*
 (* return : i *)
