aaea4f4ef109d8c36976b5c251e993ab3cf1addb
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-08-16 17:54:17 +0900

    WIP: adding vs_out everywhere does not work

--- eval8/eval.ml
+++ eval8/eval.ml
@@ -27,19 +27,19 @@ let apnd t0 t1 = match t0 with
   | Trail (h) -> cons h t1
 
 (* (>>) : i -> i -> i *)
-let (>>) i0 i1 = fun vs c a s t m ->
-  i0 vs (fun v' s' t' m' -> i1 vs c v' s' t' m') a s t m
+let (>>) i0 i1 = fun vs vs_out c a s t m ->
+  i0 vs vs_out (fun v' s' t' m' -> i1 vs vs_out c v' s' t' m') a s t m
 
 (* (>>>) : i -> i' -> i' *)
 let (>>>) i0 i1 = fun vs vs_out c a s t m ->
-  i0 vs (fun v' s' t' m' -> i1 vs_out vs c v' s' t' m') a s t m
+  i0 vs vs_out (fun v' s' t' m' -> i1 vs_out vs c v' s' t' m') a s t m
 
 (* (>>>>) : i' -> i -> i *)
-let (>>>>) i0 i1 = fun vs c a s t m ->
-  i0 vs [] (fun v' s' t' m' -> i1 vs c v' s' t' m') a s t m
+let (>>>>) i0 i1 = fun vs vs_out c a s t m ->
+  i0 vs vs_out (fun v' s' t' m' -> i1 vs vs_out c v' s' t' m') a s t m
 
 (* push : i *)
-let push = fun vs c a s t m -> c a (a :: s) t m
+let push = fun vs vs_out c a s t m -> c a (a :: s) t m
 
 (* apply8 : v -> v -> v list -> c -> s -> t -> m -> v *)
 let apply8 v0 v1 v2s c s t m = match v0 with
@@ -57,21 +57,21 @@ let ret c v (VEnv (vs_out) :: s) t m = match vs_out with
   | first :: rest -> apply8 v first rest c s t m
 
 (* num : int -> i *)
-let num n = fun vs c a s t m -> c (VNum (n)) s t m
+let num n = fun vs vs_out c a s t m -> c (VNum (n)) s t m
 
 (* num' : int -> i' *)
 let num' n = fun vs vs_out c a s t m ->
   ret c (VNum (n)) (VEnv (vs_out) :: s) t m
 
 (* access : int -> i *)
-let access n = fun vs c a s t m -> c (List.nth vs n) s t m
+let access n = fun vs vs_out c a s t m -> c (List.nth vs n) s t m
 
 (* access' : int -> i' *)
 let access' n = fun vs vs_out c a s t m ->
   ret c (List.nth vs n) (VEnv (vs_out) :: s) t m
 
 (* cur : i -> i *)
-let cur i = fun vs c a s t m ->
+let cur i = fun vs vs_out c a s t m ->
     let vfun = VFun (fun v vs_out c' s' t' m' ->
             i (v :: vs) vs_out c' v s' t' m'
         ) in
@@ -86,7 +86,7 @@ let grab i' = fun vs vs_out c a s t m ->
   end
 
 (* operation : op -> i *)
-let operation op = fun vs c v0 s t m -> match s with
+let operation op = fun vs vs_out c v0 s t m -> match s with
     v1 :: s ->
     begin match (v0, v1) with
         (VNum (n0), VNum (n1)) ->
@@ -123,7 +123,7 @@ let operation' op = fun vs vs_out c v0 s t m -> match s with
 
 (* pushmark : i *)
 (* (特に f8 において) vs_out が空であるという情報を積む *)
-let pushmark = fun vs c a s t m -> c (VEnv ([])) s t m
+let pushmark = fun vs vs_out c a s t m -> c (VEnv ([])) s t m
 
 (* mark : i' *)
 (* pushmark ではなく普通の vs_out を積む *)
@@ -131,7 +131,7 @@ let mark = fun vs vs_out c a s t m -> c (VEnv vs_out) s t m
 
 (* apply : i *)
 (* acc に v0 が積まれた状態で実行される *)
-let apply = fun vs c v0 s t m -> match s with
+let apply = fun vs vs_out c v0 s t m -> match s with
   v1 :: VEnv (v2s) :: s -> apply8 v0 v1 v2s c s t m
 
 (* appterm : i *)
@@ -139,7 +139,7 @@ let appterm = fun vs vs_out c v0 s t m -> match s with
   v1 :: VEnv (v2s) :: s -> apply8 v0 v1 v2s c s t m
 
 (* aux : i *)
-let aux = fun v2s c v1 s t m ->
+let aux = fun v2s vs_out c v1 s t m ->
   match s with VEnv (v2s) :: s ->
     c (VEnv (v1 :: v2s)) s t m
 
@@ -148,7 +148,7 @@ let aux' = fun v2s vs_out c v1 s t m ->
   match s with VEnv (v2s) :: s ->
     c (VEnv (v1 :: v2s)) s t m
 
-(* f8 : e -> string list -> i *)
+(* f8 : e -> string list -> i' *)
 let rec f8 e xs = match e with
     Num (n) -> num n
   | Var (x) -> access (Env.offset x xs)
@@ -193,7 +193,7 @@ and f8st es xs = match es with
     (f8st rest xs) >>>> push >> (f8 first xs) >>> aux'
 
 (* f : e -> v *)
-let f expr = f8 expr [] [] idc (VNum 7) [] TNil MNil
+let f expr = f8 expr [] [] [] idc (VNum 7) [] TNil MNil
 
 (*
 (* shift : i -> i *)
