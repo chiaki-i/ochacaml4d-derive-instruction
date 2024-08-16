a1e218f76d74635806696005c4ec720e35fb66ac
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-08-16 18:13:43 +0900

    eval6*: ret receives vs_out as separate arg

--- eval6b/eval.ml
+++ eval6b/eval.ml
@@ -99,14 +99,14 @@ and f6s es xs vs c a s t m = match es with
             ) (VEnv (v2s)) (VEnv (v2s) :: s2) t2 m2
       ) a s t m
 
-and ret c v (VEnv (vs_out) :: s) t m = match vs_out with (* expanding CRet (c) *)
+and ret vs_out c v s t m = match vs_out with (* expanding CRet (c) *)
     [] -> c v s t m
   | first :: rest -> apply6 v first rest c s t m
 
 and f6t e xs vs vs_out c a s t m =
   match e with
-    Num (n) -> ret c (VNum (n)) (VEnv (vs_out) :: s) t m
-  | Var (x) -> ret c (List.nth vs (Env.offset x xs)) (VEnv (vs_out) :: s) t m
+    Num (n) -> ret vs_out c (VNum (n)) s t m
+  | Var (x) -> ret vs_out c (List.nth vs (Env.offset x xs)) s t m
   | Op (e0, op, e1) ->
     f6 e1 xs vs (fun v1 s1 t1 m1 ->
             f6 e0 xs vs (fun v0 s0 t0 m0 ->
@@ -115,12 +115,12 @@ and f6t e xs vs vs_out c a s t m =
                     begin match (v0, v1) with
                         (VNum (n0), VNum (n1)) ->
                         begin match op with
-                            Plus -> ret c (VNum (n0 + n1)) (VEnv (vs_out) :: s0) t0 m0
-                          | Minus -> ret c (VNum (n0 - n1)) (VEnv (vs_out) :: s0) t0 m0
-                          | Times -> ret c (VNum (n0 * n1)) (VEnv (vs_out) :: s0) t0 m0
+                            Plus -> ret vs_out c (VNum (n0 + n1)) s0 t0 m0
+                          | Minus -> ret vs_out c (VNum (n0 - n1)) s0 t0 m0
+                          | Times -> ret vs_out c (VNum (n0 * n1)) s0 t0 m0
                           | Divide ->
                             if n1 = 0 then failwith "Division by zero"
-                            else ret c (VNum (n0 / n1)) (VEnv (vs_out) :: s0) t0 m0
+                            else ret vs_out c (VNum (n0 / n1)) s0 t0 m0
                         end
                       | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                        ^ " are not numbers")
