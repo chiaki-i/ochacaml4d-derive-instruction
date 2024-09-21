022f1dd1be9ee72873464853b804ff576e6e83a8
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-09-21 22:51:38 +0900

    eval8: add return stack redundantly

--- eval8/eval.ml
+++ eval8/eval.ml
@@ -4,13 +4,13 @@ open Value
 (* Interpreter using combinators factored as instructions : eval8 *)
 
 (* initial continuation : s -> t -> m -> v *)
-let idc v s t m = match s with
+let idc v s_a s_r t m = match s_a with
     [] ->
     begin match t with
         TNil ->
         begin match m with
             MNil -> v
-          | MCons ((c, s, t), m) -> c v s t m
+          | MCons ((c, s_a, s_r, t), m) -> c v s_a s_r t m
         end
       | Trail (h) -> h v TNil m
     end
@@ -27,63 +27,64 @@ let apnd t0 t1 = match t0 with
   | Trail (h) -> cons h t1
 
 (* (>>) : i' -> i' -> i' *)
-let (>>) i0 i1 = fun vs vs_out c a s t m ->
-  i0 vs vs_out (fun v' s' t' m' -> i1 vs vs_out c v' s' t' m') a s t m
+let (>>) i0 i1 = fun vs vs_out c a s_a s_r t m ->
+  i0 vs vs_out (fun v' s_a' s_r' t' m' -> i1 vs vs_out c v' s_a' s_r' t' m') a s_a s_r t m
 
 (* push : i' *)
-let push = fun vs vs_out c a s t m -> c a (a :: s) t m
+let push = fun vs vs_out c a s_a s_r t m -> c a (a :: s_a) s_r t m
 
 (* apply8 : v -> v -> v list -> c -> s -> t -> m -> v *)
-let apply8 v0 v1 v2s c s t m = match v0 with
-    VFun (f) -> f v1 v2s c s t m
-  | VContS (c', s', t') -> c' v1 s' t' (MCons ((c, s, t), m))
-  | VContC (c', s', t') ->
-    c' v1 s' (apnd t' (cons (fun v t m -> c v s t m) t)) m
+let apply8 v0 v1 v2s c s_a s_r t m = match v0 with
+    VFun (f) -> f v1 v2s c s_a s_r t m
+  | VContS (c', s_a', s_r', t') -> c' v1 s_a' s_r' t' (MCons ((c, s_a, s_r, t), m))
+  | VContC (c', s_a', s_r', t') ->
+    c' v1 s_a' s_r' (apnd t' (cons (fun v t m -> c v s_a s_r t m) t)) m
   | _ -> failwith (to_string v0
                     ^ " is not a function; it cannot be applied.")
 
 (* return : i' -> i' *)
-let return = fun vs vs_out c a s t m ->
-  match vs_out with
-      VEmpty :: [] -> c a s t m
-    | first :: rest -> apply8 a first rest c s t m
+let return = fun vs vs_out c a s_a s_r t m ->
+  match vs_out with (* TODO: change vs_out to s_a / s_r *)
+      VEmpty :: [] -> c a s_a s_r t m
+    | first :: rest -> apply8 a first rest c s_a s_r t m
     | [] -> failwith "return error"
 
 (* num : int -> i' *)
-let num n = fun vs vs_out c a s t m -> c (VNum (n)) s t m
+let num n = fun vs vs_out c a s_a s_r t m -> c (VNum (n)) s_a s_r t m
 
 (* access : int -> i' *)
-let access n = fun vs vs_out c a s t m -> c (List.nth vs n) s t m
+let access n = fun vs vs_out c a s_a s_r t m -> c (List.nth vs n) s_a s_r t m
 
 (* cur : i' -> i' *)
-let cur i = fun vs vs_out c a s t m ->
-    let vfun = VFun (fun v vs_out c' s' t' m' ->
-            i (v :: vs) vs_out c' v s' t' m'
+let cur i = fun vs vs_out c a s_a s_r t m ->
+    let vfun = VFun (fun v vs_out c' s_a' s_r' t' m' ->
+            i (v :: vs) vs_out c' v s_a' s_r' t' m'
         ) in
-    c vfun s t m
+    c vfun s_a s_r t m
 
 (* grab : i' -> i' *)
 (* vs_out が空かどうかによって次の instruction を実行するか決定する *)
 (* そのため grab >> (f8 e xs) とは書けない *)
-let grab i' = fun vs vs_out c a s t m ->
-  begin match vs_out with
-          [] -> c (VFun (fun v vs_out c' s' t' m' ->
-                i' (v :: vs) vs_out c' v s' t' m')) s t m (* adding vs_out, change f5 to f5t *)
-      | first :: rest -> i' (first :: vs) rest c a s t m
+let grab i' = fun vs vs_out c a s_a s_r t m ->
+  begin match vs_out with (* TODO: change vs_out to s_a / s_r *)
+        [] ->
+          c (VFun (fun v vs_out c' s_a' s_r' t' m' ->
+            i' (v :: vs) vs_out c' v s_a' s_r' t' m')) s_a s_r t m
+      | first :: rest -> i' (first :: vs) rest c a s_a s_r t m
   end
 
 (* operation : op -> i' *)
-let operation op = fun vs vs_out c v0 s t m -> match s with
-    v1 :: s ->
+let operation op = fun vs vs_out c v0 s_a s_r t m -> match s_a with
+    v1 :: s_a ->
     begin match (v0, v1) with
         (VNum (n0), VNum (n1)) ->
         begin match op with
-            Plus -> c (VNum (n0 + n1)) s t m
-          | Minus -> c (VNum (n0 - n1)) s t m
-          | Times -> c (VNum (n0 * n1)) s t m
+            Plus -> c (VNum (n0 + n1)) s_a s_r t m
+          | Minus -> c (VNum (n0 - n1)) s_a s_r t m
+          | Times -> c (VNum (n0 * n1)) s_a s_r t m
           | Divide ->
             if n1 = 0 then failwith "Division by zero"
-            else c (VNum (n0 / n1)) s t m
+            else c (VNum (n0 / n1)) s_a s_r t m
         end
       | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                        ^ " are not numbers")
@@ -92,49 +93,50 @@ let operation op = fun vs vs_out c v0 s t m -> match s with
 
 (* pushmark : i' *)
 (* (特に f8 において) vs_out が空であるという情報を積む *)
-let pushmark = fun vs vs_out c a s t m -> c (VEnv [VEmpty]) s t m
+let pushmark = fun vs vs_out c a s_a s_r t m -> c (VEnv [VEmpty]) s_a [VEmpty] t m
 
 (* mark : i' *)
+(* これ、return の 1つ目のパターン？*)
 (* pushmark ではなく普通の vs_out を積む *)
-let skip = fun vs vs_out c a s t m -> c (VEnv vs_out) s t m
+let skip = fun vs vs_out c a s_a s_r t m -> c (VEnv vs_out) s_a s_r t m (* List.hd s_r にすべき？*)
 
 (* apply : i' *)
 (* acc に v0 が積まれた状態で実行される *)
-let apply = fun vs vs_out c v0 s t m -> match s with
-  v1 :: VEnv (v2s) :: s -> apply8 v0 v1 v2s c s t m
+let apply = fun vs vs_out c v0 s_a s_r t m -> match s_a with
+  v1 :: VEnv (v2s) :: s_a -> apply8 v0 v1 v2s c s_a s_r t m
 
 (* appterm : i' *)
-let appterm = fun vs vs_out c v0 s t m -> match s with
-  v1 :: VEnv (v2s) :: s -> apply8 v0 v1 v2s c s t m
+let appterm = fun vs vs_out c v0 s_a s_r t m -> match s_a with
+  v1 :: VEnv (v2s) :: s_a -> apply8 v0 v1 v2s c s_a s_r t m
 
 (* aux : i' *)
-let aux = fun v2s vs_out c v1 s t m ->
-  match s with VEnv (v2s) :: s ->
-    c (VEnv (v1 :: v2s)) s t m
+let aux = fun v2s vs_out c v1 s_a s_r t m ->
+  match s_a with VEnv (v2s) :: s_a ->
+    c (VEnv (v1 :: v2s)) s_a s_r t m
 
 (* shift : i' -> i' *)
-let shift i = fun vs vs_out c a s t m ->
-  i (VContS (c, s, t) :: vs) vs_out idc a [] TNil m
+let shift i = fun vs vs_out c a s_a s_r t m ->
+  i (VContS (c, s_a, s_r, t) :: vs) vs_out idc a [] [] TNil m
 
 (* control : i' -> i' *)
-let control i = fun vs vs_out c a s t m ->
-  i (VContC (c, s, t) :: vs) vs_out idc a [] TNil m
+let control i = fun vs vs_out c a s_a s_r t m ->
+  i (VContC (c, s_a, s_r, t) :: vs) vs_out idc a [] [] TNil m
 
 (* shift0 : i' -> i' *)
-let shift0 i = fun vs vs_out c a s t m -> match m with
-    MCons ((c0, s0, t0), m0) ->
-    i (VContS (c, s, t) :: vs) vs_out c0 a s0 t0 m0
+let shift0 i = fun vs vs_out c a s_a s_r t m -> match m with
+    MCons ((c0, s0_a, s0_r, t0), m0) ->
+    i (VContS (c, s_a, s_r, t) :: vs) vs_out c0 a s0_a s0_r t0 m0
   | _ -> failwith "shift0 is used without enclosing reset"
 
 (* control0 : i' -> i' *)
-let control0 i = fun vs vs_out c a s t m -> match m with
-    MCons ((c0, s0, t0), m0) ->
-    i (VContC (c, s, t) :: vs) vs_out c0 a s0 t0 m0
+let control0 i = fun vs vs_out c a s_a s_r t m -> match m with
+    MCons ((c0, s0_a, s0_r, t0), m0) ->
+    i (VContC (c, s_a, s_r, t) :: vs) vs_out c0 a s0_a s0_r t0 m0
   | _ -> failwith "control0 is used without enclosing reset"
 
 (* reset : i' -> i' *)
-let reset i = fun vs vs_out c a s t m ->
-  i vs vs_out idc a [] TNil (MCons ((c, s, t), m))
+let reset i = fun vs vs_out c a s_a s_r t m ->
+  i vs vs_out idc a [] [] TNil (MCons ((c, s_a, s_r, t), m))
 
 (* f8 : e -> string list -> i' *)
 let rec f8 e xs = match e with
@@ -179,4 +181,4 @@ and f8st es xs = match es with
     (f8st rest xs) >> push >> (f8 first xs) >> aux
 
 (* f : e -> v *)
-let f expr = f8 expr [] [] [] idc (VNum 7) [] TNil MNil
+let f expr = f8 expr [] [] [] idc (VNum 7) [] [] TNil MNil
--- eval8/value.ml
+++ eval8/value.ml
@@ -2,24 +2,23 @@
 
 (* Value *)
 type v = VNum of int
-       | VFun of (v -> v list -> c -> s -> t -> m -> v)
-       | VContS of c * s * t
-       | VContC of c * s * t
+       | VFun of (v -> v list -> c -> s -> s -> t -> m -> v)
+       | VContS of c * s * s * t
+       | VContC of c * s * s * t
        | VEnv of v list
        | VEmpty
-       (* | VK of c *)
 
-and c = v -> s -> t -> m -> v
+and c = v -> s -> s -> t -> m -> v
 
 and s = v list
 
 and t = TNil | Trail of (v -> t -> m -> v)
 
 and m = MNil
-      | MCons of (c * s * t) * m
+      | MCons of (c * s * s * t) * m
 
 (* type i  = v list -> c -> v -> s -> t -> m -> v *)
-type i' = v list -> v list -> c -> v -> s -> t -> m -> v (* for tail call optimization *)
+type i' = v list -> v list -> c -> v -> s -> s -> t -> m -> v
 
 (* to_string : v -> string *)
 let rec to_string value = match value with
@@ -28,7 +27,7 @@ let rec to_string value = match value with
   | VContS (_) -> "<VContS>"
   | VContC (_) -> "<VContC>"
   | VEnv (_) -> "<VEnv>"
-(*   | VK (_) -> "<VK>" *)
+  | VEmpty -> "<VEmpty>"
 
 let vlist_of_string lst =
   List.fold_left (fun s1 s2 -> s1 ^ " " ^ s2) "" (List.map to_string lst)
