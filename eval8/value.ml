cce82f70169e375943a1105a93aa7b525de58976
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-11-09 16:23:27 +0900

    revert 022f1dd

--- eval8/value.ml
+++ eval8/value.ml
@@ -2,23 +2,24 @@
 
 (* Value *)
 type v = VNum of int
-       | VFun of (v -> v list -> c -> s -> s -> t -> m -> v)
-       | VContS of c * s * s * t
-       | VContC of c * s * s * t
+       | VFun of (v -> v list -> c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
        | VEnv of v list
        | VEmpty
+       (* | VK of c *)
 
-and c = v -> s -> s -> t -> m -> v
+and c = v -> s -> t -> m -> v
 
 and s = v list
 
 and t = TNil | Trail of (v -> t -> m -> v)
 
 and m = MNil
-      | MCons of (c * s * s * t) * m
+      | MCons of (c * s * t) * m
 
 (* type i  = v list -> c -> v -> s -> t -> m -> v *)
-type i' = v list -> v list -> c -> v -> s -> s -> t -> m -> v
+type i' = v list -> v list -> c -> v -> s -> t -> m -> v (* for tail call optimization *)
 
 (* to_string : v -> string *)
 let rec to_string value = match value with
@@ -27,7 +28,7 @@ let rec to_string value = match value with
   | VContS (_) -> "<VContS>"
   | VContC (_) -> "<VContC>"
   | VEnv (_) -> "<VEnv>"
-  | VEmpty -> "<VEmpty>"
+(*   | VK (_) -> "<VK>" *)
 
 let vlist_of_string lst =
   List.fold_left (fun s1 s2 -> s1 ^ " " ^ s2) "" (List.map to_string lst)
