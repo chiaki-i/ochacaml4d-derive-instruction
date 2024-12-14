2edb998e58b7bdbe8080f83dc11044d44f25be47
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-12-14 22:37:33 +0900

    WIP: eval9: defunctionalizing num, access, op

--- eval9/eval.ml
+++ eval9/eval.ml
@@ -3,88 +3,59 @@ open Value
 
 (* Defunctionalized interpreter : eval9 *)
 
-(* cons : h -> t -> t *)
-let cons h t = match t with
+(* cons : (v -> t -> m -> v) -> t -> t *)
+let rec cons h t = match t with
     TNil -> Trail (h)
-  | Trail (h') -> Trail (Append (h, h'))
+  | Trail (h') -> Trail (fun v t' m -> h v (cons h' t') m)
 
 (* apnd : t -> t -> t *)
 let apnd t0 t1 = match t0 with
     TNil -> t1
   | Trail (h) -> cons h t1
 
-(* run_h9 : h -> v -> t -> m -> v *)
-let rec run_h9 h v t m = match h with
-    Hold (c, s) -> run_c9 c (v :: s) t m
-  | Append (h, h') -> run_h9 h v (cons h' t) m
-
 (* run_c9 : c -> s -> t -> m -> v *)
-and run_c9 c s t m = match c with
-    [] ->
-    begin match s with
-        v :: [] ->
-        begin match t with
-            TNil ->
-            begin match m with
-                [] -> v
-              | (c, s, t) :: m -> run_c9 c (v :: s) t m
-            end
-          | Trail (h) -> run_h9 h v TNil m
+let rec run_c9 c s r t m = match (c, s, r) with
+    ([], v :: [], []) ->
+    begin match t with
+        TNil ->
+        begin match m with
+            [] -> v
+          | (c, s, r, t) :: m -> run_c9 c (v :: s) r t m
         end
-      | _ -> failwith "stack error"
+      | Trail (h) -> h v TNil m
     end
-  | i :: c -> run_i9 i c s t m
+  | (i1 :: c, v :: s, VEnv (vs) :: r) ->
+    run_i9 i1 vs c (v :: s) r t m
+  | _ -> failwith "run_c9 unexpected"
 
 (* run_i9 : i -> c -> s -> t -> m -> v *)
-and run_i9 i c s t m = match i with
-    INum (n) ->
-    begin match s with
-        VEnv (vs) :: s -> run_c9 c (VNum (n) :: s) t m
-      | _ -> failwith "stack error"
+and run_i9 i vs c s r t m = match i with
+    IVArg ->
+    begin match s with (v :: s) ->
+        run_c9 c (VArg (v) :: s) r t m
+      | _ -> failwith "stack error: run_i9 varg"
     end
-  | IAccess (n) ->
-    begin match s with
-        VEnv (vs) :: s -> run_c9 c ((List.nth vs n) :: s) t m
-      | _ -> failwith "stack error"
-    end
-  | IPush_closure (i) ->
-    begin match s with
-        VEnv (vs) :: s -> run_c9 c (VFun (i, vs) :: s) t m
-      | _ -> failwith "stack error"
-    end
-  | IReturn ->
-    begin match s with
-        v :: VK (c') :: s -> run_c9 c' (v :: s) t m
-      | _ -> failwith "stack error"
-    end
-  | IPush_env ->
-    begin match s with
-        VEnv (vs) :: s -> run_c9 c (VEnv (vs) :: VEnv (vs) :: s) t m
-      | _ -> failwith "stack error"
-    end
-  | IPop_env ->
-    begin match s with
-        v :: VEnv (vs) :: s -> run_c9 c (VEnv (vs) :: v :: s) t m
-      | _ -> failwith "stack error"
-    end
+  | INum (n) -> run_c9 c (VNum (n) :: s) r t m
+  | IAccess (n) -> run_c9 c ((List.nth vs n) :: s) r t m
   | IOp (op) ->
-    begin match s with
-        v1 :: v0 :: s ->
+    begin match (s, r) with
+        (v0 :: v1 :: s, r) ->
         begin match (v0, v1) with
             (VNum (n0), VNum (n1)) ->
             begin match op with
-                Plus -> run_c9 c (VNum (n0 + n1) :: s) t m
-              | Minus -> run_c9 c (VNum (n0 - n1) :: s) t m
-              | Times -> run_c9 c (VNum (n0 * n1) :: s) t m
+                Plus -> run_c9 c (VNum (n0 + n1) :: s) r t m
+              | Minus -> run_c9 c (VNum (n0 - n1) :: s) r t m
+              | Times -> run_c9 c (VNum (n0 * n1) :: s) r t m
               | Divide ->
                 if n1 = 0 then failwith "Division by zero"
-                else run_c9 c (VNum (n0 / n1) :: s) t m
+                else run_c9 c (VNum (n0 / n1) :: s) r t m
             end
           | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                            ^ " are not numbers")
         end
       | _ -> failwith "stack error"
     end
+  (*
   | ICall ->
     begin match s with
         v1 :: v0 :: s ->
@@ -94,66 +65,74 @@ and run_i9 i c s t m = match i with
           | VContC (c', s', t') ->
             run_c9 c' (v1 :: s') (apnd t' (cons (Hold (c, s)) t)) m
           | _ -> failwith (to_string v0
-                           ^ " is not a function; it can not be applied.")
+                          ^ " is not a function; it can not be applied.")
         end
       | _ -> failwith "stack error"
     end
-  | IShift (i) ->
-    begin match s with
-        VEnv (vs) :: s ->
-        run_i9 i [] (VEnv (VContS (c, s, t) :: vs) :: []) TNil m
-      | _ -> failwith "stack error"
-    end
-  | IControl (i) ->
-    begin match s with
-        VEnv (vs) :: s ->
-        run_i9 i [] (VEnv (VContC (c, s, t) :: vs) :: []) TNil m
-      | _ -> failwith "stack error"
-    end
-  | IShift0 (i) ->
-    begin match s with
-        VEnv (vs) :: s ->
-        begin match m with
-            (c0, s0, t0) :: m0 ->
-            run_i9 i c0 (VEnv (VContS (c, s, t) :: vs) :: s0) t0 m0
-          | _ -> failwith "shift0 is used without enclosing reset"
-        end
-      | _ -> failwith "stack error"
-    end
-  | IControl0 (i) ->
-    begin match s with
-        VEnv (vs) :: s ->
-        begin match m with
-            (c0, s0, t0) :: m0 ->
-            run_i9 i c0 (VEnv (VContC (c, s, t) :: vs) :: s0) t0 m0
-          | _ -> failwith "control0 is used without enclosing reset"
-        end
-      | _ -> failwith "stack error"
-    end
-  | IReset (i) ->
-    begin match s with
-        VEnv (vs) :: s ->
-        run_i9 i [] (VEnv (vs) :: []) TNil ((c, s, t) :: m)
-      | _ -> failwith "stack error"
-    end
-  | ISeq (i0, i1) -> run_i9 i0 (i1 :: c) s t m
+    *)
+    (*
+    | IShift (i) ->
+      begin match s with
+          VEnv (vs) :: s ->
+          run_i9 i [] (VEnv (VContS (c, s, t) :: vs) :: []) TNil m
+        | _ -> failwith "stack error"
+      end
+    | IControl (i) ->
+      begin match s with
+          VEnv (vs) :: s ->
+          run_i9 i [] (VEnv (VContC (c, s, t) :: vs) :: []) TNil m
+        | _ -> failwith "stack error"
+      end
+    | IShift0 (i) ->
+      begin match s with
+          VEnv (vs) :: s ->
+          begin match m with
+              (c0, s0, t0) :: m0 ->
+              run_i9 i c0 (VEnv (VContS (c, s, t) :: vs) :: s0) t0 m0
+            | _ -> failwith "shift0 is used without enclosing reset"
+          end
+        | _ -> failwith "stack error"
+      end
+    | IControl0 (i) ->
+      begin match s with
+          VEnv (vs) :: s ->
+          begin match m with
+              (c0, s0, t0) :: m0 ->
+              run_i9 i c0 (VEnv (VContC (c, s, t) :: vs) :: s0) t0 m0
+            | _ -> failwith "control0 is used without enclosing reset"
+          end
+        | _ -> failwith "stack error"
+      end
+    | IReset (i) ->
+      begin match s with
+          VEnv (vs) :: s ->
+          run_i9 i [] (VEnv (vs) :: []) TNil ((c, s, t) :: m)
+        | _ -> failwith "stack error"
+      end *)
+  | ISeq (i0, i1) ->
+    run_i9 i0 vs (i1 :: c) s (VEnv (vs) :: r) t m
+  | _ -> failwith "not implemented"
 
 (* (>>) : i -> i -> i *)
 let (>>) i0 i1 = ISeq (i0, i1)
 
 (* f9 : e -> string list -> i *)
-let rec f9 e xs = match e with
+let rec f9 e xs = begin match e with
     Num (n) -> INum (n)
   | Var (x) -> IAccess (Env.offset x xs)
   | Op (e0, op, e1) ->
-    IPush_env >> (f9 e0 xs) >> IPop_env >> (f9 e1 xs) >> IOp (op)
+    (f9 e0 xs) >> (f9 e1 xs) >> IOp (op)
+  (*
   | Fun (x, e) -> IPush_closure ((f9 e (x :: xs)) >> IReturn)
   | App (e0, e1) -> IPush_env >> (f9 e0 xs) >> IPop_env >> (f9 e1 xs) >> ICall
   | Shift (x, e) -> IShift (f9 e (x :: xs))
   | Control (x, e) -> IControl (f9 e (x :: xs))
   | Shift0 (x, e) -> IShift0 (f9 e (x :: xs))
   | Control0 (x, e) -> IControl0 (f9 e (x :: xs))
-  | Reset (e) -> IReset (f9 e xs) 
-           
+  | Reset (e) -> IReset (f9 e xs)
+  *)
+  | _ -> failwith "not implemented"
+  end
+
 (* f : e -> v *)
-let f expr = run_i9 (f9 expr []) [] (VEnv ([]) :: []) TNil []
+let f expr = run_i9 (f9 expr []) [] [] [] [] TNil []
--- eval9/value.ml
+++ eval9/value.ml
@@ -8,26 +8,38 @@ type v = VNum of int
        | VContS of c * s * t
        | VContC of c * s * t
        | VEnv of v list
-       | VK of c
+       | VArg of v
 
-and i = INum of int | IAccess of int
+and i = IVArg
+      | INum of int
+      | IAccess of int
+      | IOp of op
+      (* | IApply
+      | IGrab of i *)
+      | ISeq of i * i (* >> の実体 *)
+
+(* and i = INum of int | IAccess of int
       | IPush_closure of i | IReturn
       | IPush_env | IPop_env | IOp of op | ICall
       | IShift of i | IControl of i
       | IShift0 of i | IControl0 of i
       | IReset of i
-      | ISeq of i * i
+      | ISeq of i * i *)
 
 and c = i list
 
 and s = v list
 
-and h = Hold of c * s
+and r = v list
+
+(* and h = Hold of c * s
       | Append of h * h
 
-and t = TNil | Trail of h
+and t = TNil | Trail of h *)
 
-type m = (c * s * t) list
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = (c * s * r * t) list (* M をリストにした理由は？ *)
 
 
 (* to_string : v -> string *)
@@ -37,7 +49,18 @@ let rec to_string value = match value with
   | VContS (_) -> "<VContS>"
   | VContC (_) -> "<VContC>"
   | VEnv (_) -> "<VEnv>"
-  | VK (_) -> "<VK>"
+  | VArg (v) -> "<VArg: " ^ to_string v ^ ">"
+
+(* s_to_string : v -> string *)
+let rec s_to_string s =
+  "[" ^
+  begin match s with
+    [] -> ""
+  | first :: rest ->
+    to_string first ^
+    List.fold_left (fun str v -> str ^ "; " ^ to_string v) "" rest
+  end
+  ^ "]"
 
 (* Value.print : v -> unit *)
 let print exp =
