a53d2a370c23cddf3e291297ecc49d6acee13633
Kenichi Asai <asai@is.ocha.ac.jp>
2024-12-29 16:06:00 +0900

    eval7ds: arguments passed via stack

---
+++ eval7ds/value.ml
@@ -0,0 +1,45 @@
+open Syntax
+
+(* Defunctionalized interpreter with values passed via stack : eval7d wo r.s.*)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VArgs of v list
+
+and c = C0
+      | CApp0 of c
+      | CApp1 of e * string list * v list * c
+      | CAppS0 of cs * c
+      | CApplyS of c
+      | COp0 of op * c
+      | COp1 of e * string list * v list * op * c
+
+and s = v list
+
+and cs = CApp2 of e * e * string list * v list
+       | CAppS1 of e * string list * v list * cs
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs ([]) -> "[]"
+  | VArgs (v :: vs) ->
+    "[" ^ List.fold_left (fun s v -> s ^ "; " ^ to_string v)
+                         (to_string v) vs ^ "]"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
