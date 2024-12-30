853516c57c33926b3f7e77760f6373e80a6923b2
Chiaki Ishio <chiaki.ishio@gmail.com>
2024-12-30 23:58:14 +0900

    eval9sr: copy from eval9s

---
+++ eval9sr/value.ml
@@ -0,0 +1,60 @@
+open Syntax
+
+(* Interpreter using combinators factored as instructions : eval8s *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of i * v list
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VArgs of v list
+
+and c = (i * v list) list
+
+and i = IPush
+      | IPushmark
+      | INum of int
+      | IAccess of int
+      | IOp of op
+      | IApply
+      | IFun of i
+      | ISeq of i * i (* >> の実体 *)
+      | IShift of i | IControl of i
+      | IShift0 of i | IControl0 of i
+      | IReset of i
+
+and s = v list
+
+and h = Hold of c * s
+      | Append of h * h
+
+and t = TNil | Trail of h
+
+and m = (c * s * t) list
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs ([]) -> "[]"
+  | VArgs (v :: vs) ->
+    "[" ^ List.fold_left (fun s v -> s ^ "; " ^ to_string v)
+                         (to_string v) vs ^ "]"
+
+(* s_to_string : v -> string *)
+let rec s_to_string s =
+  "[" ^
+  begin match s with
+    [] -> ""
+  | first :: rest ->
+    to_string first ^
+    List.fold_left (fun str v -> str ^ "; " ^ to_string v) "" rest
+  end
+  ^ "]"
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
