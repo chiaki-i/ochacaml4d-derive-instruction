dade6ade4899b39f949255783faafee0bb939d10
Kenichi Asai <asai@is.ocha.ac.jp>
2024-12-30 12:49:48 +0900

    eval8s: combinators as instructions

---
+++ eval8s/value.ml
@@ -0,0 +1,39 @@
+open Syntax
+
+(* Interpreter using combinators factored as instructions : eval8s *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VArgs of v list
+
+and c = C0
+      | CSeq of i * v list * c
+
+and i = v list -> c -> s -> t -> m -> v
+
+and s = v list
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs ([]) -> "[]"
+  | VArgs (v :: vs) ->
+    "[" ^ List.fold_left (fun s v -> s ^ "; " ^ to_string v)
+                         (to_string v) vs ^ "]"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
