6d745eb5629af79fe8a535a50edc93b2d3fbdf3b
Kenichi Asai <asai@is.ocha.ac.jp>
2024-12-30 11:33:31 +0900

    eval7ds4: refunctionalize c'

---
+++ eval7ds4/value.ml
@@ -0,0 +1,44 @@
+open Syntax
+
+(* Defunctionalized interpreter with values passed via stack : eval7d wo r.s.*)
+(* refunctionalize c' *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VArgs of v list
+
+and c = C0
+      | CSeq of c' * v list * c
+      | CAppS0 of v list * cs
+
+and c' = v list -> c -> s -> t -> m -> v
+
+and s = v list
+
+and cs = CApp2 of e * string list * v list * c
+       | CAppS1 of e * string list * v list * cs
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs ([]) -> "[]"
+  | VArgs (v :: vs) ->
+    "[" ^ List.fold_left (fun s v -> s ^ "; " ^ to_string v)
+                         (to_string v) vs ^ "]"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
