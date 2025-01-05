c4f776d295cc82a6ccc4cd56564b587041e30ec2
Kenichi Asai <asai@is.ocha.ac.jp>
2025-01-05 14:22:11 +0900

    eval10sv: inline run_i9 into run_c9 still with ISeq

---
+++ eval10sv/value.ml
@@ -0,0 +1,55 @@
+open Syntax
+
+(* Defuntionalized instructions : eval9sv *)
+(* Derived from eval8sv *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (c -> s -> r -> t -> m -> v)
+       | VContS of c * s * r * t
+       | VContC of c * s * r * t
+       | VArgs of v list
+
+and c = i list
+
+and i = IPush
+      | IPushmark
+      | INum of int
+      | IAccess of int
+      | IOp of op
+      | IApply
+      | IFun of i
+      | IReturn
+      | ISeq of i * i
+      | IShift of i | IControl of i
+      | IShift0 of i | IControl0 of i
+      | IReset of i
+
+and s = v list
+
+and r = rv list
+
+and rv = VS of v list
+       | VK of c * r
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * r * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs ([]) -> "[]"
+  | VArgs (v :: vs) ->
+    "[" ^ List.fold_left (fun s v -> s ^ "; " ^ to_string v)
+                         (to_string v) vs ^ "]"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
