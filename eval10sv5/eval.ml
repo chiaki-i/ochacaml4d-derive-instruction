b1a79a5c77c43eabe81a6ebd1e98ddb592a9b3ac
Kenichi Asai <asai@is.ocha.ac.jp>
2025-01-14 20:57:05 +0900

    eval10sv5: IApply: case dispatch on v0, inline apply9

--- eval10sv5/eval.ml
+++ eval10sv5/eval.ml
@@ -1,6 +1,7 @@
 open Syntax
 open Value
 
+(* IApply: case dispatch on v0, inline apply9 *)
 (* IApply: case dispatch on v2s, inline apply9s *)
 (* linearize continuations : eval10sv4 *)
 (* Derived from eval10sv3 *)
@@ -59,9 +60,19 @@ let rec run_c9 c s t m = match (c, s) with
     end
   | ((IApply :: is, VS (vs)) :: c, v0 :: VArgs ([]) :: s) ->
     run_c9 ((is, VS (vs)) :: c) (v0 :: s) t m
-  | ((IApply :: is, VS (vs)) :: c, v0 :: VArgs (v1 :: v2s) :: s) ->
-    apply9 v0 v1 (([IApply], VS (vs)) :: (is, VS (vs)) :: c)
-           (VArgs (v2s) :: s) t m
+  | ((IApply :: is, VS (vs)) :: c, VFun (f) :: VArgs (v1 :: v2s) :: s) ->
+    f (([IApply], VS (vs)) :: (is, VS (vs)) :: c) (v1 :: VArgs (v2s) :: s) t m
+  | ((IApply :: is, VS (vs)) :: c,
+     VContS (c', s', t') :: VArgs (v1 :: v2s) :: s) ->
+    run_c9 c' (v1 :: s') t'
+           (MCons ((([IApply], VS (vs)) :: (is, VS (vs)) :: c,
+                    VArgs (v2s) :: s, t), m))
+  | ((IApply :: is, VS (vs)) :: c,
+     VContC (c', s', t') :: VArgs (v1 :: v2s) :: s) ->
+    run_c9 c' (v1 :: s')
+           (apnd t' (cons (fun v t m ->
+             run_c9 (([IApply], VS (vs)) :: (is, VS (vs)) :: c)
+                    (v :: VArgs (v2s) :: s) t m) t)) m
   | ((IFun (i) :: IApply :: is, VS (vs)) :: c', VArgs (v1 :: v2s) :: s') ->
           (* Grab *)
           (* print_endline ("grab: " ^ Value.s_to_string s); *)
@@ -97,17 +108,6 @@ let rec run_c9 c s t m = match (c, s) with
     end
   | ((IReset (i) :: is, VS (vs)) :: c, s) ->
     run_c9 ((i, VS (vs)) :: idc) [] TNil (MCons (((is, VS (vs)) :: c, s, t), m))
-
-(* apply9 : v -> v -> c -> s -> t -> m -> v *)
-and apply9 v0 v1 c s t m = match v0 with
-    VFun (f) -> f c (v1 :: s) t m
-  | VContS (c', s', t') ->
-    run_c9 c' (v1 :: s') t' (MCons ((c, s, t), m))
-  | VContC (c', s', t') ->
-    run_c9 c' (v1 :: s')
-           (apnd t' (cons (fun v t m -> run_c9 c (v :: s) t m) t)) m
-  | _ -> failwith (to_string v0
-                   ^ " is not a function; it can not be applied.")
 
 (* f9 : e -> string list -> i list *)
 let rec f9 e xs = match e with
