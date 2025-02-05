adf74843e508045939f0e5b499715f84b5a282a9
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-02-05 22:25:59 +0900

    eval7st: copied from eval7st

---
+++ eval7st1/value.ml
@@ -0,0 +1,48 @@
+open Syntax
+
+(* interpreter with arg stack, with value on top of it: eval7st *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (v -> c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VArgs of v list
+
+and c = C0
+      | CApp0 of c
+      | CAppS0 of c
+      | COp0 of op * c
+      | COp1 of e * string list * op * v list * c
+      | CAppT1 of e * string list * v list * c
+      | CAppS1 of e * string list * v list * c
+
+and s = v list
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VArgs (_) -> "<VArgs>"
+
+(* s_to_string : s -> string *)
+let rec s_to_string s =
+  "[" ^
+  begin match s with
+    [] -> ""
+  | first :: rest ->
+    to_string first ^
+    List.fold_left (fun str v -> str ^ "; " ^ to_string v) "" rest
+  end
+  ^ "]"
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
