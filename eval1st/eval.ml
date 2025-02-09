6f5b6b8ae73f93a061fe948953ccf3b66ecac75d
Kenichi Asai <asai@is.ocha.ac.jp>
2025-02-09 10:06:22 +0900

    eval1st: introduce grab optimization

--- eval1st/eval.ml
+++ eval1st/eval.ml
@@ -47,11 +47,10 @@ let rec f1 e xs vs c t m =
             end) t0 m0) t m
   | Fun (x, e) ->
     c (VFun (fun v1 c' t' m' ->
-              f1t e (x :: xs) (v1 :: vs) c' t' m')) t m
+              f1 e (x :: xs) (v1 :: vs) c' t' m')) t m
   | App (e0, e2s) ->
     f1s e2s xs vs (fun v2s t2 m2 ->
-      f1t e0 xs vs (fun v0 t0 m0 -> (* 試験的に、関数部分は f1t で再帰 *)
-        apply1s v0 v2s c t0 m0) t2 m2) t m
+      f1t e0 xs vs v2s c t2 m2) t m
   | Shift (x, e) -> f1 e (x :: xs) (VContS (c, t) :: vs) idc TNil m
   | Control (x, e) -> f1 e (x :: xs) (VContC (c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
@@ -68,58 +67,62 @@ let rec f1 e xs vs c t m =
     end
   | Reset (e) -> f1 e xs vs idc TNil (MCons ((c, t), m))
 
-(* f1s : e list -> string list -> v list -> c -> t -> m -> v list *)
-(* f1s は関数適用の引数部分を実行しているので、f1t を再帰呼び出しする必要はない *)
-and f1s e2s xs vs c t m = match e2s with
-    [] -> c [] t m
-  | e :: e2s ->
-    f1s e2s xs vs (fun v2s t2 m2 ->
-      f1 e xs vs (fun v1 t1 m1 ->
-        c (v1 :: v2s) t1 m1) t2 m2) t m
-
-(* f1t : e -> string list -> v list -> c -> t -> m -> v *)
-and f1t e xs vs c t m =
+(* f1t : e -> string list -> v list -> v list -> c -> t -> m -> v *)
+and f1t e xs vs v2s c t m =
+  let ret_c v0 t0 m0 = apply1s v0 v2s c t0 m0 in
   match e with
-    Num (n) -> c (VNum (n)) t m
-  | Var (x) -> c (List.nth vs (Env.offset x xs)) t m
+    Num (n) -> ret_c (VNum (n)) t m
+  | Var (x) -> ret_c (List.nth vs (Env.offset x xs)) t m
   | Op (e0, op, e1) ->
     f1 e1 xs vs (fun v1 t0 m0 ->
         f1 e0 xs vs (fun v0 t1 m1 ->
             begin match (v0, v1) with
                 (VNum (n0), VNum (n1)) ->
                 begin match op with
-                    Plus -> c (VNum (n0 + n1)) t1 m1
-                  | Minus -> c (VNum (n0 - n1)) t1 m1
-                  | Times -> c (VNum (n0 * n1)) t1 m1
+                    Plus -> ret_c (VNum (n0 + n1)) t1 m1
+                  | Minus -> ret_c (VNum (n0 - n1)) t1 m1
+                  | Times -> ret_c (VNum (n0 * n1)) t1 m1
                   | Divide ->
                     if n1 = 0 then failwith "Division by zero"
-                    else c (VNum (n0 / n1)) t1 m1
+                    else ret_c (VNum (n0 / n1)) t1 m1
                 end
               | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                ^ " are not numbers")
             end) t0 m0) t m
   | Fun (x, e) ->
-    c (VFun (fun v1 c' t' m' ->
-              f1t e (x :: xs) (v1 :: vs) c' t' m')) t m
+    begin match v2s with
+      [] -> c (VFun (fun v1 c' t' m' -> f1 e (x :: xs) (v1 :: vs) c' t' m'))
+              t m
+ (* | v1 :: v2s -> f1 e (x :: xs) (v1 :: vs)
+                      (fun v t m -> apply1s v v2s c t m) t m *)
+    | v1 :: v2s -> f1t e (x :: xs) (v1 :: vs) v2s c t m
+    end
   | App (e0, e2s) ->
     f1s e2s xs vs (fun v2s t2 m2 ->
-      f1t e0 xs vs (fun v0 t0 m0 -> (* 関数適用の関数部分を f1t で最適化 *)
-        apply1s v0 v2s c t0 m0) t2 m2) t m
-  | Shift (x, e) -> f1 e (x :: xs) (VContS (c, t) :: vs) idc TNil m
-  | Control (x, e) -> f1 e (x :: xs) (VContC (c, t) :: vs) idc TNil m
+      f1t e0 xs vs v2s c t2 m2) t m
+  | Shift (x, e) -> f1 e (x :: xs) (VContS (ret_c, t) :: vs) idc TNil m
+  | Control (x, e) -> f1 e (x :: xs) (VContC (ret_c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f1 e (x :: xs) (VContS (c, t) :: vs) c0 t0 m0
+          f1 e (x :: xs) (VContS (ret_c, t) :: vs) c0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f1 e (x :: xs) (VContC (c, t) :: vs) c0 t0 m0
+          f1 e (x :: xs) (VContC (ret_c, t) :: vs) c0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f1 e xs vs idc TNil (MCons ((c, t), m))
+  | Reset (e) -> f1 e xs vs idc TNil (MCons ((ret_c, t), m))
+
+(* f1s : e list -> string list -> v list -> c -> t -> m -> v list *)
+and f1s e2s xs vs c t m = match e2s with
+    [] -> c [] t m
+  | e :: e2s ->
+    f1s e2s xs vs (fun v2s t2 m2 ->
+      f1 e xs vs (fun v1 t1 m1 ->
+        c (v1 :: v2s) t1 m1) t2 m2) t m
 
 (* apply1 : v -> v -> c -> t -> m -> v *)
 and apply1 v0 v1 c t m = match v0 with
