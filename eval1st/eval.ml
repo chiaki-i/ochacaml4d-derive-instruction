a6ecf4c9ff6d62ff442e7a5fce4f96b3bf4694f8
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-03-15 13:14:28 +0900

    eval1st: rename ret_c to app_c

--- eval1st/eval.ml
+++ eval1st/eval.ml
@@ -69,22 +69,22 @@ let rec f1 e xs vs c t m =
 
 (* f1t : e -> string list -> v list -> v list -> c -> t -> m -> v *)
 and f1t e xs vs v2s c t m =
-  let ret_c v0 t0 m0 = apply1s v0 v2s c t0 m0 in
+  let app_c v0 t0 m0 = apply1s v0 v2s c t0 m0 in
   match e with
-    Num (n) -> ret_c (VNum (n)) t m
-  | Var (x) -> ret_c (List.nth vs (Env.offset x xs)) t m
+    Num (n) -> app_c (VNum (n)) t m
+  | Var (x) -> app_c (List.nth vs (Env.offset x xs)) t m
   | Op (e0, op, e1) ->
     f1 e1 xs vs (fun v1 t0 m0 ->
         f1 e0 xs vs (fun v0 t1 m1 ->
             begin match (v0, v1) with
                 (VNum (n0), VNum (n1)) ->
                 begin match op with
-                    Plus -> ret_c (VNum (n0 + n1)) t1 m1
-                  | Minus -> ret_c (VNum (n0 - n1)) t1 m1
-                  | Times -> ret_c (VNum (n0 * n1)) t1 m1
+                    Plus -> app_c (VNum (n0 + n1)) t1 m1
+                  | Minus -> app_c (VNum (n0 - n1)) t1 m1
+                  | Times -> app_c (VNum (n0 * n1)) t1 m1
                   | Divide ->
                     if n1 = 0 then failwith "Division by zero"
-                    else ret_c (VNum (n0 / n1)) t1 m1
+                    else app_c (VNum (n0 / n1)) t1 m1
                 end
               | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                ^ " are not numbers")
@@ -97,22 +97,22 @@ and f1t e xs vs v2s c t m =
     end
   | App (e0, e2s) ->
     f1s e2s xs vs (fun v2s t2 m2 ->
-      f1t e0 xs vs v2s ret_c t2 m2) t m
-  | Shift (x, e) -> f1 e (x :: xs) (VContS (ret_c, t) :: vs) idc TNil m
-  | Control (x, e) -> f1 e (x :: xs) (VContC (ret_c, t) :: vs) idc TNil m
+      f1t e0 xs vs v2s app_c t2 m2) t m
+  | Shift (x, e) -> f1 e (x :: xs) (VContS (app_c, t) :: vs) idc TNil m
+  | Control (x, e) -> f1 e (x :: xs) (VContC (app_c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f1 e (x :: xs) (VContS (ret_c, t) :: vs) c0 t0 m0
+          f1 e (x :: xs) (VContS (app_c, t) :: vs) c0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f1 e (x :: xs) (VContC (ret_c, t) :: vs) c0 t0 m0
+          f1 e (x :: xs) (VContC (app_c, t) :: vs) c0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f1 e xs vs idc TNil (MCons ((ret_c, t), m))
+  | Reset (e) -> f1 e xs vs idc TNil (MCons ((app_c, t), m))
 
 (* f1s : e list -> string list -> v list -> c -> t -> m -> v list *)
 and f1s e2s xs vs c t m = match e2s with
