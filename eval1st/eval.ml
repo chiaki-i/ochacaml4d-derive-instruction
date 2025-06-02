a3fa83f8e32993e52449731d33ec19510e1b6b20
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-06-03 00:48:40 +0900

    WIP: adding app_c in Fun (x, e) of f1 should work... 5 test remaining

--- eval1st/eval.ml
+++ eval1st/eval.ml
@@ -47,7 +47,8 @@ let rec f1 e xs vs c t m =
             end) t0 m0) t m
   | Fun (x, e) ->
     c (VFun (fun v1 v2s c' t' m' ->
-              f1t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
+              f1t e (x :: xs) (v1 :: vs) v2s
+                (fun v t m -> apply1s v v2s c' t m) t' m')) t m
   | App (e0, e2s) ->
     f1s e2s xs vs (fun (v1 :: v2s) t2 m2 ->
       f1 e0 xs vs (fun v0 t0 m0 ->
@@ -72,30 +73,29 @@ let rec f1 e xs vs c t m =
 and f1t e xs vs v2s c t m =
   let app_c v0 t0 m0 = apply1s v0 v2s c t0 m0 in
   match e with
-    Num (n) -> app_c (VNum (n)) t m
-  | Var (x) -> app_c (List.nth vs (Env.offset x xs)) t m
+    Num (n) -> c (VNum (n)) t m
+    (* これは、何もしてないのではなくて、v2s = [] という前提のもとで
+       app_c (VNum (n)) t m をインライン展開した形 *)
+  | Var (x) -> c (List.nth vs (Env.offset x xs)) t m
   | Op (e0, op, e1) ->
     f1 e1 xs vs (fun v1 t0 m0 ->
         f1 e0 xs vs (fun v0 t1 m1 ->
             begin match (v0, v1) with
                 (VNum (n0), VNum (n1)) ->
                 begin match op with
-                    Plus -> app_c (VNum (n0 + n1)) t1 m1
-                  | Minus -> app_c (VNum (n0 - n1)) t1 m1
-                  | Times -> app_c (VNum (n0 * n1)) t1 m1
+                    Plus -> c (VNum (n0 + n1)) t1 m1
+                  | Minus -> c (VNum (n0 - n1)) t1 m1
+                  | Times -> c (VNum (n0 * n1)) t1 m1
                   | Divide ->
                     if n1 = 0 then failwith "Division by zero"
-                    else app_c (VNum (n0 / n1)) t1 m1
+                    else c (VNum (n0 / n1)) t1 m1
                 end
               | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
                                ^ " are not numbers")
             end) t0 m0) t m
   | Fun (x, e) ->
-    begin match v2s with
-      [] -> c (VFun (fun v1 v2s c' t' m' ->
-                       f1t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
-    | v1 :: v2s -> f1t e (x :: xs) (v1 :: vs) v2s c t m
-    end
+    app_c (VFun (fun v1 v2s c' t' m' ->
+              f1t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
   | App (e0, e2s) ->
     f1s e2s xs vs (fun (v1 :: v2s) t2 m2 ->
       f1 e0 xs vs (fun v0 t0 m0 ->
@@ -126,15 +126,17 @@ and f1s e2s xs vs c t m = match e2s with
         c (v1 :: v2s) t1 m1) t2 m2) t m
 
 (* apply1 : v -> v -> v list -> c -> t -> m -> v *)
-and apply1 v0 v1 v2s c t m =
-  let app_c = fun v t m -> apply1s v v2s c t m in
-  match v0 with
+and apply1 v0 v1 v2s c t m = match v0 with
     VFun (f) -> f v1 v2s c t m
-  | VContS (c', t') -> c' v1 t' (MCons ((app_c, t), m))
+  | VContS (c', t') ->
     (* c’の最後に return 的なことができないか？
       関数化して、VFun (f) のように、直接 f を呼び出す格好になると、shift の実装の中で展開して最適化できそう
       MCons なども f の中で実装（というか適用）すれば良い *)
-  | VContC (c', t') -> c' v1 (apnd t' (cons app_c t)) m
+    let app_c = fun v t m -> apply1s v v2s c t m in
+    c' v1 t' (MCons ((app_c, t), m))
+  | VContC (c', t') ->
+    let app_c = fun v t m -> apply1s v v2s c t m in
+    c' v1 (apnd t' (cons app_c t)) m
   | _ -> failwith (to_string v0
                    ^ " is not a function; it can not be applied.")
 
