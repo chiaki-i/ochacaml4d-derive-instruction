8e2df54ae4f39c268012ac8d6b30e7bdc4f8c4c7
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-06-04 00:16:03 +0900

    eval1st1: copied from eval1st: going to introduce epsilon in apply1s

---
+++ eval1st1/value.ml
@@ -0,0 +1,30 @@
+open Syntax
+
+(* Definitional interpreter for λ-calculus with 4 delimited continuation operations
+  with tail optimization : eval1st *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (v -> v list -> c -> t -> m -> v)
+       | VContS of c * t (*(v -> v list -> c -> t -> m -> v) *)
+       | VContC of c * t
+
+and c = v -> t -> m -> v
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
