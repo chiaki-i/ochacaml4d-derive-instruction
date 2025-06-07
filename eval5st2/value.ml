0fab5f48114046e24b615344a47a7c14dce9dff1
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-06-07 15:24:05 +0900

    eval5st2: copied from eval5st1

---
+++ eval5st2/value.ml
@@ -0,0 +1,59 @@
+open Syntax
+
+(* interpreter with defunctionalized continuations: eval2st *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (v -> c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VEmpty
+
+and c = C0
+      | CApp of c
+      | CApp1 of c
+      | CAppS0 of c
+      | CAppS1 of e * string list * v list * c
+      | CAppS2 of e * string list * v list * c
+      | COp0 of op * c
+      | COp1 of e * string list * op * v list * c
+
+and s = v list
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VEmpty -> "<ε>"
+
+(* s_to_string : s -> string *)
+let rec s_to_string s =
+  "[" ^
+  begin match s with
+    [] -> ""
+  | first :: rest ->
+    to_string first ^
+    List.fold_left (fun str v -> str ^ "; " ^ to_string v) "" rest
+  end
+  ^ "]"
+
+(* c_to_string : c -> string *)
+let rec c_to_string cont = match cont with
+    C0 -> "<C0>"
+  | CApp (_) -> "<CApp>"
+  | CAppS1 (_, _, _, _) -> "<CAppS1>"
+  | CAppS2 (_, _, _, _) -> "<CAppS2>"
+  | CAppS0 (_) -> "<CAppS0>"
+  | COp0 (_, _) -> "<COp0>"
+  | COp1 (_, _, _, _, _) -> "<COp1>"
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
