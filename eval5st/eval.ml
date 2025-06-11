8a79cce86afaa98e115f018da5f48d15da88d2d2
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-06-11 17:46:23 +0900

    eval5st: s added redundantly from eval2st; s does not carry any args

--- eval5st/eval.ml
+++ eval5st/eval.ml
@@ -1,8 +1,8 @@
 open Syntax
 open Value
 
-(* defunctionalized interpreter with argument stack: eval5st *)
-(* eval5st is without explicit return stack since c will represent one instead *)
+(* defunctionalized interpreter with redundant argument stack: eval5st *)
+(* this time, arg stack doesn't carry any args, it's just an empty stack *)
 
 (* initial continuation *)
 let idc = C0
@@ -29,8 +29,8 @@ let rec run_c5 c v s t m = match (c, s) with
       | Trail (h) -> h v TNil m
     end
   | (COp1 (e0, xs, op, vs, c), s) ->
-    f5 e0 xs vs (COp0 (op, c)) (v :: s) t m
-  | (COp0 (op, c), v0 :: s) ->
+    f2 e0 xs vs (COp0 (v, op, c)) s t m
+  | (COp0 (v0, op, c), s) ->
     begin match (v, v0) with
         (VNum (n0), VNum (n1)) ->
         begin match op with
@@ -43,105 +43,104 @@ let rec run_c5 c v s t m = match (c, s) with
         end
       | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
     end
-  | (CApp (c), VArgs (v2s) :: s) -> apply5s v v2s c s t m
-  | (CApp1 (c), v1 :: VArgs (v2s) :: s) ->
-    apply5 v v1 c (VArgs (v2s) :: s) t m
-  | (CAppS0 (cs), VArgs (v2s) :: s) -> run_c5s cs (v :: v2s) s t m
+  | (CApp (v2s, c), s) -> apply2s v v2s c s t m
+  | (CApp1 (v1, v2s, c), s) -> apply2 v v1 v2s c s t m
+  | (CAppS0 (v2s, cs), s) -> run_c5s cs (v :: v2s) s t m
   | _ -> failwith "run_c5: unexpected c"
 
-(* run_c5s : cs -> v list -> s -> t -> m -> v *)
+(* run_c5s : cs -> v list -> t -> m -> v *)
 and run_c5s c v2s s t m = match (c, s) with
     (CAppS1 (e, xs, vs, c), s) ->
-    f5 e xs vs (CAppS0 (c)) (VArgs (v2s) :: s) t m
+    f2 e xs vs (CAppS0 (v2s, c)) s t m
   | (CAppS2 (e, xs, vs, c), s) ->
     begin match v2s with (v1 :: v2s) ->
-        f5 e xs vs (CApp1 (c)) (v1 :: VArgs (v2s) :: s) t m
+        f2 e xs vs (CApp1 (v1, v2s, c)) s t m
       | _ -> failwith "run_c5s: unexpected v2s"
     end
   | _ -> failwith "run_c5s: unexpected c"
 
-(* f5: defunctionalized interpreter *)
-(* f5 : e -> string list -> v list -> c -> s -> t -> m -> v *)
-and f5 e xs vs c s t m = match e with
+(* f2: defunctionalized interpreter *)
+(* f2 : e -> string list -> v list -> c -> t -> m -> v *)
+and f2 e xs vs c s t m = match e with
     Num (n) -> run_c5 c (VNum (n)) s t m
   | Var (x) -> run_c5 c (List.nth vs (Env.offset x xs)) s t m
-  | Op (e0, op, e1) -> f5 e1 xs vs (COp1 (e0, xs, op, vs, c)) s t m
+  | Op (e0, op, e1) ->
+    f2 e1 xs vs (COp1 (e0, xs, op, vs, c)) s t m
   | Fun (x, e) ->
-    run_c5 c (VFun (fun v1 c' s' t' m' ->
-      f5t e (x :: xs) (v1 :: vs) c' s' t' m')) s t m
+    run_c5 c (VFun (fun v1 v2s c' s' t' m' ->
+      f2t e (x :: xs) (v1 :: vs) v2s c' s' t' m')) s t m
   | App (e0, e2s) ->
-    f5s e2s xs vs (CAppS2 (e0, xs, vs, c)) s t m
-  | Shift (x, e) -> f5 e (x :: xs) (VContS (c, s, t) :: vs) idc [] TNil m
-  | Control (x, e) -> f5 e (x :: xs) (VContC (c, s, t) :: vs) idc [] TNil m
+    f2s e2s xs vs (CAppS2 (e0, xs, vs, c)) s t m
+  | Shift (x, e) ->
+    f2 e (x :: xs) (VContS (c, s, t) :: vs) idc s TNil m
+  | Control (x, e) ->
+    f2 e (x :: xs) (VContC (c, s, t) :: vs) idc s TNil m
   | Shift0 (x, e) ->
     begin match m with
         MCons ((c0, s0, t0), m0) ->
-        f5 e (x :: xs) (VContS (c, s, t) :: vs) c0 s0 t0 m0
+        f2 e (x :: xs) (VContS (c, s, t) :: vs) c0 s0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
         MCons ((c0, s0, t0), m0) ->
-        f5 e (x :: xs) (VContC (c, s, t) :: vs) c0 s0 t0 m0
+        f2 e (x :: xs) (VContC (c, s, t) :: vs) c0 s0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f5 e xs vs idc [] TNil (MCons ((c, s, t), m))
+  | Reset (e) -> f2 e xs vs idc s TNil (MCons ((c, s, t), m))
 
-(* f5s : e list -> string list -> v list -> cs -> s -> t -> m -> v list *)
-and f5s e2s xs vs cs s t m = match e2s with
+(* f2s : e list -> string list -> v list -> cs -> t -> m -> v list *)
+and f2s e2s xs vs cs s t m = match e2s with
     [] -> run_c5s cs [] s t m
   | e :: e2s ->
-    f5s e2s xs vs (CAppS1 (e, xs, vs, cs)) s t m
+    f2s e2s xs vs (CAppS1 (e, xs, vs, cs)) s t m
 
-(* f5t : e -> string list -> v list -> c -> s -> t -> m -> v *)
-and f5t e xs vs v2s c s t m =
-  let (app_c, app_s) = (CApp (c), VArgs (v2s) :: s) in
+(* f2t : e -> string list -> v list -> v list -> c -> t -> m -> v *)
+and f2t e xs vs v2s c s t m =
+  let app_c = CApp (v2s, c) in
   match e with
-    Num (n) -> run_c5 app_c (VNum (n)) app_s t m
-  | Var (x) -> run_c5 app_c (List.nth vs (Env.offset x xs)) app_s t m
-  | Op (e0, op, e1) -> f5 e1 xs vs (COp1 (e0, xs, op, vs, app_c)) app_s t m
+    Num (n) -> run_c5 app_c (VNum (n)) s t m
+  | Var (x) -> run_c5 app_c (List.nth vs (Env.offset x xs)) s t m
+  | Op (e0, op, e1) -> f2 e1 xs vs (COp1 (e0, xs, op, vs, app_c)) s t m
   | Fun (x, e) ->
     begin match v2s with
       [] ->
-      run_c5 c (VFun (fun v1 c' t' m' ->
-        f5t e (x :: xs) (v1 :: vs) c' t' m')) s t m
-    | v1 :: v2s -> f5t e (x :: xs) (v1 :: vs) v2s c s t m
+      run_c5 c (VFun (fun v1 v2s c' s' t' m' ->
+        f2t e (x :: xs) (v1 :: vs) v2s c' s' t' m')) s t m
+    | v1 :: v2s -> f2t e (x :: xs) (v1 :: vs) v2s c s t m
     end
   | App (e0, e2s) ->
-    f5s e2s xs vs (CAppS2 (e0, xs, vs, app_c)) app_s t m
-  | Shift (x, e) ->
-    f5 e (x :: xs) (VContS (app_c, app_s, t) :: vs) idc [] TNil m
-  | Control (x, e) ->
-    f5 e (x :: xs) (VContC (app_c, app_s, t) :: vs) idc [] TNil m
+    f2s e2s xs vs (CAppS2 (e0, xs, vs, app_c)) s t m
+  | Shift (x, e) -> f2 e (x :: xs) (VContS (app_c, s, t) :: vs) idc s TNil m
+  | Control (x, e) -> f2 e (x :: xs) (VContC (app_c, s, t) :: vs) idc s TNil m
   | Shift0 (x, e) ->
     begin match m with
         MCons ((c0, s0, t0), m0) ->
-        f5 e (x :: xs) (VContS (app_c, app_s, t) :: vs) c0 s0 t0 m0
+          f2 e (x :: xs) (VContS (app_c, s, t) :: vs) c0 s0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
         MCons ((c0, s0, t0), m0) ->
-        f5 e (x :: xs) (VContC (app_c, app_s, t) :: vs) c0 s0 t0 m0
+          f2 e (x :: xs) (VContC (app_c, s, t) :: vs) c0 s0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f5 e xs vs idc [] TNil (MCons ((app_c, app_s, t), m))
+  | Reset (e) -> f2 e xs vs idc s TNil (MCons ((app_c, s, t), m))
 
-(* apply5 : v -> v -> c -> s -> t -> m -> v *)
-and apply5 v0 v1 c (VArgs (v2s) :: s) t m =
-  let (app_c, app_s) = (CApp (c), VArgs (v2s) :: s) in
+(* apply2 : v -> v -> v list -> c -> t -> m -> v *)
+and apply2 v0 v1 v2s c s t m =
+  let app_c = CApp (v2s, c) in
   match v0 with
     VFun (f) -> f v1 v2s c s t m
-  | VContS (c', s', t') -> run_c5 c' v1 s' t' (MCons ((app_c, app_s, t), m))
-  | VContC (c', s', t') ->
-    run_c5 c' v1 s' (apnd t' (cons (fun v t m -> run_c5 app_c v app_s t m) t)) m
+  | VContS (c', s', t') -> run_c5 c' v1 s' t' (MCons ((app_c, s, t), m))
+  | VContC (c', s', t') -> run_c5 c' v1 s' (apnd t' (cons (fun v t m -> run_c5 app_c v s t m) t)) m
   | _ -> failwith (to_string v0
                    ^ " is not a function; it can not be applied.")
 
-(* apply5s : v -> v list -> c -> s -> t -> m -> v *)
-and apply5s v0 v2s c s t m = match v2s with
+(* apply2s : v -> v list -> c -> t -> m -> v *)
+and apply2s v0 v2s c s t m = match v2s with
     [] -> run_c5 c v0 s t m
-  | v1 :: v2s -> apply5 v0 v1 c (VArgs (v2s) :: s) t m
+  | v1 :: v2s -> apply2 v0 v1 v2s c s t m
 
 (* f : e -> v *)
-let f expr = f5 expr [] [] idc [] TNil MNil
+let f expr = f2 expr [] [] idc [] TNil MNil
--- eval5st/value.ml
+++ eval5st/value.ml
@@ -7,15 +7,15 @@ type v = VNum of int
        | VFun of (v -> v list -> c -> s -> t -> m -> v)
        | VContS of c * s * t
        | VContC of c * s * t
-       | VArgs of v list
+       | VEmpty
 
 and c = C0
-      | CApp of c
-      | CApp1 of c
-      | CAppS0 of c
+      | CApp of v list * c
+      | CApp1 of v * v list * c
+      | CAppS0 of v list * c
       | CAppS1 of e * string list * v list * c
       | CAppS2 of e * string list * v list * c
-      | COp0 of op * c
+      | COp0 of v * op * c
       | COp1 of e * string list * op * v list * c
 
 and s = v list
@@ -30,7 +30,7 @@ let rec to_string value = match value with
   | VFun (_) -> "<VFun>"
   | VContS (_) -> "<VContS>"
   | VContC (_) -> "<VContC>"
-  | VArgs (_) -> "<VArgs>"
+  | VEmpty -> "<ε>"
 
 (* s_to_string : s -> string *)
 let rec s_to_string s =
@@ -44,14 +44,14 @@ let rec s_to_string s =
   ^ "]"
 
 (* c_to_string : c -> string *)
-let rec c_to_string cont = match cont with
+(* let rec c_to_string cont = match cont with
     C0 -> "<C0>"
   | CApp (_) -> "<CApp>"
   | CAppS1 (_, _, _, _) -> "<CAppS1>"
   | CAppS2 (_, _, _, _) -> "<CAppS2>"
   | CAppS0 (_) -> "<CAppS0>"
   | COp0 (_, _) -> "<COp0>"
-  | COp1 (_, _, _, _, _) -> "<COp1>"
+  | COp1 (_, _, _, _, _) -> "<COp1>" *)
 
 (* Value.print : v -> unit *)
 let print exp =
