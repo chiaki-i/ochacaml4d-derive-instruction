5a0d1bc4d940dc27ab7404d45fb256fc9b433468
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-06-12 13:13:27 +0900

    eval10st1: return stack optimization

--- eval10st1/eval.ml
+++ eval10st1/eval.ml
@@ -6,9 +6,6 @@ open Value
 (* initial continuation *)
 let idc = []
 
-(* mark on arg stack *)
-let mark = VArgs ([])
-
 (* cons : (v -> t -> m -> v) -> t -> t *)
 let rec cons h t = match t with
     TNil -> Trail (h)
@@ -19,7 +16,6 @@ let apnd t0 t1 = match t0 with
     TNil -> t1
   | Trail (h) -> cons h t1
 
-(* run_c10: c -> s -> t -> m -> v *)
 let rec run_c10 c s t m = match (c, s) with
     ([], v :: []) ->
     begin match t with
@@ -54,39 +50,32 @@ let rec run_c10 c s t m = match (c, s) with
     end
   | ((ICur (is') :: is, vs) :: c, s) ->
     run_c10 ((is, vs) :: c) (VFun (is', vs) :: s) t m
-  | ((IGrab (is') :: is, vs) :: c, s) ->
-    begin match s with (VArgs (v2s) :: s) ->
-        begin match v2s with
-          [] ->
-          run_c10 ((is, vs) :: c) (VFun (is', vs) :: s) t m
-        | v1 :: v2s ->
-          run_c10 ((is', (v1 :: vs)) :: (is, vs) :: c) (VArgs (v2s) :: s) t m
-        end
+  | ((IGrab (is') :: [], vs) :: c, s) ->
+    begin match s with
+        VEmpty :: s ->
+        run_c10 c (VFun (is', vs) :: s) t m
+      | v1 :: s ->
+        run_c10 ((is', (v1 :: vs)) :: c) s t m
       | _ -> failwith "IGrab: unexpected s"
     end
   | ((IApply :: is, vs) :: c, s) ->
-    begin match s with (v :: VArgs ([]) :: s) ->
-        run_c10 ((is, vs) :: c) (v :: s) t m
-      | (v :: VArgs (v1 :: v2s) :: s) ->
-        let app_c = ([IApply], vs) :: (is, vs) :: c in
-        let app_s = VArgs (v2s) :: s in
-        begin match v with
-            VFun (is', vs') -> run_c10 ((is', (v1 :: vs')) :: (is, vs) :: c) (VArgs (v2s) :: s) t m
-          | VContS (c', s', t') -> run_c10 c' (v1 :: s') t' (MCons ((app_c, app_s, t), m))
-          | VContC (c', s', t') ->
-            run_c10 c' (v1 :: s') (apnd t' (cons (fun v t m -> run_c10 app_c (v :: app_s) t m) t)) m
-          | _ -> failwith (to_string v
-                          ^ " is not a function; it can not be applied.")
-        end
+    begin match s with (v :: v1 :: s) ->
+        apply10 v v1 vs ((is, vs) :: c) s t m
       | _ -> failwith "IApply: unexpected s"
     end
-  | ((IPushmark :: is, vs) :: c, s) ->
-    run_c10 ((is, vs) :: c) (mark :: s) t m
-  | ((IPush :: is, vs) :: c, s) ->
-    begin match s with v :: VArgs (v2s) :: s ->
-        run_c10 ((is, vs) :: c) (VArgs (v :: v2s) :: s) t m
-      | _ -> failwith "IPush: unexpected s"
+  | ((IAppterm :: [], vs) :: c, s) ->
+    begin match s with (v :: v1 :: s) ->
+        let app_c = ([IReturn], vs) :: c in
+        apply10 v v1 vs app_c s t m
+      | _ -> failwith "IAppterm: unexpected s"
     end
+  | ((IReturn :: [], vs) :: c, s) ->
+    begin match s with (v :: s) ->
+        apply10s v vs c s t m
+      | _ -> failwith "IReturn: unexpected s"
+    end
+  | ((IPushmark :: is, vs) :: c, s) ->
+    run_c10 ((is, vs) :: c) (VEmpty :: s) t m
   | ((IShift (i) :: is, vs) :: c, s) ->
     run_c10
       ((i, VContS (((is, vs) :: c), s, t) :: vs) :: idc)
@@ -117,6 +106,24 @@ let rec run_c10 c s t m = match (c, s) with
       [] TNil (MCons ((((is, vs) :: c), s, t), m))
   | _ -> failwith "run_c10: stack error"
 
+(* apply10 : v -> v -> v list -> c -> s -> t -> m -> v *)
+and apply10 v0 v1 vs c s t m =
+  match v0 with
+    VFun (is, vs') -> run_c10 ((is, (v1 :: vs')) :: c) s t m
+  | VContS (c', s', t') ->
+    let app_c = ([IReturn], vs) :: c in
+    run_c10 c' (v1 :: s') t' (MCons ((app_c, s, t), m))
+  | VContC (c', s', t') ->
+    let app_c = ([IReturn], vs) :: c in
+    run_c10 c' (v1 :: s') (apnd t' (cons (fun v t m -> run_c10 app_c (v :: s) t m) t)) m
+  | _ -> failwith (to_string v0
+                   ^ " is not a function; it can not be applied.")
+
+(* apply10s : v -> v list -> c -> s -> t -> m -> v *)
+and apply10s v0 vs c s t m = match s with
+    VEmpty :: s -> run_c10 c (v0 :: s) t m
+  | v1 :: s -> apply10 v0 v1 vs c s t m
+
 (* f10: e -> string list -> i list *)
 let rec f10 e xs = match e with
     Num (n) -> [INum (n)]
@@ -125,7 +132,7 @@ let rec f10 e xs = match e with
     f10 e1 xs @ f10 e0 xs @ [IOp (op)]
   | Fun (x, e) -> [ICur (f10t e (x :: xs))]
   | App (e0, e2s) ->
-    f10s e2s xs @ f10t e0 xs
+    f10s e2s xs @ f10 e0 xs @ [IApply]
   | Shift (x, e) -> [IShift (f10 e (x :: xs))]
   | Control (x, e) -> [IControl (f10 e (x :: xs))]
   | Shift0 (x, e) -> [IShift0 (f10 e (x :: xs))]
@@ -135,21 +142,21 @@ let rec f10 e xs = match e with
 (* f10s : e list -> string list -> i list *)
 and f10s e2s xs = match e2s with
     [] -> [IPushmark]
-  | e :: e2s -> f10s e2s xs @ f10 e xs @ [IPush]
+  | e :: e2s -> f10s e2s xs @ f10 e xs
 
 (* f10t : e -> string list -> i list *)
 and f10t e xs = match e with
-    Num (n) -> [INum n; IApply]
-  | Var (x) -> [IAccess (Env.offset x xs); IApply]
+    Num (n) -> [INum n; IReturn]
+  | Var (x) -> [IAccess (Env.offset x xs); IReturn]
   | Op (e0, op, e1) ->
-    f10 e1 xs @ f10 e0 xs @ [IOp (op); IApply]
+    f10 e1 xs @ f10 e0 xs @ [IOp (op); IReturn]
   | Fun (x, e) -> [IGrab (f10t e (x :: xs))]
-  | App (e0, e2s) -> f10s e2s xs @ f10t e0 xs @ [IApply]
-  | Shift (x, e) -> [IShift (f10 e (x :: xs)); IApply]
-  | Control (x, e) -> [IControl (f10 e (x :: xs)); IApply]
-  | Shift0 (x, e) -> [IShift0 (f10 e (x :: xs)); IApply]
-  | Control0 (x, e) -> [IControl0 (f10 e (x :: xs)); IApply]
-  | Reset (e) -> [IReset (f10 e xs); IApply]
+  | App (e0, e2s) -> f10s e2s xs @ f10 e0 xs @ [IAppterm]
+  | Shift (x, e) -> [IShift (f10 e (x :: xs)); IReturn]
+  | Control (x, e) -> [IControl (f10 e (x :: xs)); IReturn]
+  | Shift0 (x, e) -> [IShift0 (f10 e (x :: xs)); IReturn]
+  | Control0 (x, e) -> [IControl0 (f10 e (x :: xs)); IReturn]
+  | Reset (e) -> [IReset (f10 e xs); IReturn]
 
 (* f : e -> v *)
 let f expr = run_c10 ((f10 expr [], []) :: []) [] TNil MNil
--- eval10st1/value.ml
+++ eval10st1/value.ml
@@ -7,7 +7,7 @@ type v = VNum of int
        | VFun of i list * v list
        | VContS of c * s * t
        | VContC of c * s * t
-       | VArgs of v list
+       | VEmpty
 
 and c = (i list * v list) list
 
@@ -17,7 +17,8 @@ and i = IPush
       | IAccess of int
       | IOp of op
       | IApply
-      | IAppterm of i list
+      | IReturn
+      | IAppterm
       | ICur of i list
       | IGrab of i list
       | IShift of i list | IControl of i list
@@ -36,7 +37,7 @@ let rec to_string value = match value with
   | VFun (_) -> "<VFun>"
   | VContS (_) -> "<VContS>"
   | VContC (_) -> "<VContC>"
-  | VArgs (_) -> "<VArgs>"
+  | VEmpty -> "<ε>"
 
 (* s_to_string : s -> string *)
 let rec s_to_string s =
