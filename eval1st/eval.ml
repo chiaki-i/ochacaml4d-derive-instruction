182e364db975693273ae5c92c375d2491b682900
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-08-03 23:22:16 +0900

    [WIP] eval1st: cannot pass v2s to f1sr ...

--- eval1st/eval.ml
+++ eval1st/eval.ml
@@ -60,13 +60,14 @@ let rec f1 e xs vs c t m =
     (* f1 e (x :: xs) (VContS ((fun v v2s t (MCons ((c', t'), m'))-> apply1s v v2s c' t' m'), t) :: vs) idc TNil m *)
     (* 上は step2 のボツ案; これは c そのものを変更せねばならず影響範囲が大きすぎる *)
     (* begin match m with (* VContS - step2 採用案: v2s を MCons に積む *)
-        MCons ((_, v2s, _), _) -> (* v2s を取り出して *)
-        f1 e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s c t' m'), t) :: vs) idc TNil m (* v2s を apply1s に渡す *)
+        MCons ((_, v2s', _), _) -> (* v2s を取り出して *)
+        f1 e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s' c t' m'), t) :: vs) idc TNil m (* v2s を apply1s に渡す *)
       | MNil -> failwith "shift: unexpected m"
     end *)
     begin match m with
-        MCons ((_, v2s, _), _) -> (* v2s を取り出して *)
-        f1 e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s c t' m'), t) :: vs) idc TNil m (* v2s を apply1s に渡す *)
+        MCons ((_, v2s', _), _) -> (* v2s を取り出して *)
+        f1sr e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s' c t' m'), t) :: vs) idc TNil m
+        (* v2s を f1sr に渡そうとしたが、肝心の v2s は VContS の中にいるので、外に出せなくない？ VFun の時とは事情が違う… *)
       | MNil -> failwith "shift: unexpected m"
     end
   | Control (x, e) -> f1 e (x :: xs) (VContC (c, t) :: vs) idc TNil m
@@ -135,6 +136,57 @@ and f1t e xs vs v2s c t m =
     end
   | Reset (e) -> f1 e xs vs idc TNil (MCons ((app_c, [], t), m))
 
+and f1sr e xs vs v2s c t m =
+  let app_c = fun v0 t0 m0 -> apply1s v0 v2s c t0 m0 in
+  match e with
+    Num (n) -> app_c (VNum (n)) t m
+  | Var (x) -> app_c (List.nth vs (Env.offset x xs)) t m
+  | Op (e0, op, e1) ->
+    f1 e1 xs vs (fun v1 t0 m0 ->
+        f1 e0 xs vs (fun v0 t1 m1 ->
+            begin match (v0, v1) with
+                (VNum (n0), VNum (n1)) ->
+                begin match op with
+                    Plus -> app_c (VNum (n0 + n1)) t1 m1
+                  | Minus -> app_c (VNum (n0 - n1)) t1 m1
+                  | Times -> app_c (VNum (n0 * n1)) t1 m1
+                  | Divide ->
+                    if n1 = 0 then failwith "Division by zero"
+                    else app_c (VNum (n0 / n1)) t1 m1
+                end
+              | _ -> failwith (to_string v0 ^ " or " ^ to_string v1
+                               ^ " are not numbers")
+            end) t0 m0) t m
+  | Fun (x, e) ->
+    begin match v2s with
+        [] -> c (VFun (fun v1 v2s c' t' m' ->
+          f1t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
+      | v1 :: v2s -> f1t e (x :: xs) (v1 :: vs) v2s c t m
+    end
+  | App (e0, e2s) ->
+    f1s e2s xs vs (fun (v1 :: v2s) t2 m2 ->
+      f1 e0 xs vs (fun v0 t0 m0 ->
+        apply1 v0 v1 v2s app_c t0 m0) t2 m2) t m
+  | Shift (x, e) ->
+    (* f1 e (x :: xs) (VContS (app_c, t) :: vs) idc TNil m *)
+    f1 e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s c t' m'), t) :: vs) idc TNil m (* VContS - step 2 *)
+  | Control (x, e) -> f1 e (x :: xs) (VContC (app_c, t) :: vs) idc TNil m
+  | Shift0 (x, e) ->
+    begin match m with
+        MCons ((c0, v2s', t0), m0) ->
+          f1 e (x :: xs) (VContS ((fun v t' m' -> apply1s v v2s' c t' m'), t) :: vs) c0 t0 m0
+          (* ↑ VContS - step 2: v2s' でも v2s でも結果同じなのでテストケース拡充した方が良いかも *)
+      | _ -> failwith "shift0 is used without enclosing reset"
+    end
+  | Control0 (x, e) ->
+    begin match m with
+        MCons ((c0, _, t0), m0) ->
+          f1 e (x :: xs) (VContC (app_c, t) :: vs) c0 t0 m0
+      | _ -> failwith "control0 is used without enclosing reset"
+    end
+  | Reset (e) -> f1 e xs vs idc TNil (MCons ((app_c, [], t), m))
+
+
 (* f1s : e list -> string list -> v list -> c -> t -> m -> v list *)
 and f1s e2s xs vs c t m = match e2s with
     [] -> c [] t m
@@ -148,7 +200,7 @@ and apply1 v0 v1 v2s c t m = match v0 with
     VFun (f) -> f v1 v2s c t m
   | VContS (c', t') ->
     (* c' v1 t' (MCons (((fun v t m -> apply1s v v2s c t m), t), m)) *) (* VContS - step1 *)
-    c' v1 t' (MCons ((c, v2s, t), m))
+    c' v1 t' (MCons ((c, v2s, t), m)) (* MCons の v2s に実質的な中身を積んでいるのはここ *)
   | VContC (c', t') ->
     let app_c = fun v t m -> apply1s v v2s c t m in
     c' v1 (apnd t' (cons app_c t)) m
