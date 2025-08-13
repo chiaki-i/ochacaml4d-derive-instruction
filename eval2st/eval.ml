21abf1bd34e21eb9e9040228cc4be7c4189f2e78
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-08-13 16:33:44 +0900

    eval2st

--- eval2st/eval.ml
+++ eval2st/eval.ml
@@ -23,7 +23,9 @@ let rec run_c2 c v t m = match c with
         TNil ->
         begin match m with
             MNil -> v
-          | MCons ((c, t), m) -> run_c2 c v t m
+          | MCons ((c, v2s, t), m) ->
+            let app_c0 = CApp (v2s, c) in
+            run_c2 app_c0 v t m
         end
       | Trail (h) -> h v TNil m
     end
@@ -71,15 +73,17 @@ and f2 e xs vs c t m = match e with
   | Control (x, e) -> f2 e (x :: xs) (VContC (c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
     begin match m with
-        MCons ((c0, t0), m0) -> f2 e (x :: xs) (VContS (c, t) :: vs) c0 t0 m0
+        MCons ((c0, v2s, t0), m0) ->
+          f2sr e (x :: xs) (VContS (c, t) :: vs) v2s c0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
-        MCons ((c0, t0), m0) -> f2 e (x :: xs) (VContC (c, t) :: vs) c0 t0 m0
+        MCons ((c0, v2s, t0), m0) ->
+          f2sr e (x :: xs) (VContC (c, t) :: vs) v2s c0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f2 e xs vs idc TNil (MCons ((c, t), m))
+  | Reset (e) -> f2 e xs vs idc TNil (MCons ((c, [], t), m))
 
 (* f2s : e list -> string list -> v list -> cs -> t -> m -> v list *)
 and f2s e2s xs vs cs t m = match e2s with
@@ -107,24 +111,53 @@ and f2t e xs vs v2s c t m =
   | Control (x, e) -> f2 e (x :: xs) (VContC (app_c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
     begin match m with
-        MCons ((c0, t0), m0) ->
-          f2 e (x :: xs) (VContS (app_c, t) :: vs) c0 t0 m0
+        MCons ((c0, v2s', t0), m0) ->
+          f2sr e (x :: xs) (VContS (app_c, t) :: vs) v2s' c0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
-        MCons ((c0, t0), m0) ->
-          f2 e (x :: xs) (VContC (app_c, t) :: vs) c0 t0 m0
+        MCons ((c0, v2s', t0), m0) ->
+          f2sr e (x :: xs) (VContC (app_c, t) :: vs) v2s' c0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f2 e xs vs idc TNil (MCons ((app_c, t), m))
+  | Reset (e) -> f2 e xs vs idc TNil (MCons ((c, v2s, t), m))
+
+(* f2sr : e -> string list -> v list -> v list -> c -> t -> m -> v *)
+and f2sr e xs vs v2s c t m =
+  let app_c0 = CApp (v2s, c) in
+  match e with
+    Num (n) -> run_c2 app_c0 (VNum (n)) t m
+  | Var (x) -> run_c2 app_c0 (List.nth vs (Env.offset x xs)) t m
+  | Op (e0, op, e1) ->
+    f2 e1 xs vs (COp1 (e0, xs, op, vs, app_c0)) t m
+  | Fun (x, e) ->
+    run_c2 app_c0 (VFun (fun v1 v2s c' t' m' ->
+              f2t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
+  | App (e0, e2s) ->
+    f2s e2s xs vs (CAppS2 (e0, xs, vs, app_c0)) t m
+  | Shift (x, e) -> f2 e (x :: xs) (VContS (app_c0, t) :: vs) idc TNil m
+  | Control (x, e) -> f2 e (x :: xs) (VContC (app_c0, t) :: vs) idc TNil m
+  | Shift0 (x, e) ->
+    begin match m with
+        MCons ((c0, v2s, t0), m0) ->
+          f2sr e (x :: xs) (VContS (app_c0, t) :: vs) v2s c0 t0 m0
+      | _ -> failwith "shift0 is used without enclosing reset"
+    end
+  | Control0 (x, e) ->
+    begin match m with
+        MCons ((c0, v2s, t0), m0) ->
+          f2sr e (x :: xs) (VContC (app_c0, t) :: vs) v2s c0 t0 m0
+      | _ -> failwith "control0 is used without enclosing reset"
+    end
+  | Reset (e) -> f2 e xs vs idc TNil (MCons ((app_c0, [], t), m))
 
 (* apply2 : v -> v -> v list -> c -> t -> m -> v *)
 and apply2 v0 v1 v2s c t m =
   let app_c = CApp (v2s, c) in
   match v0 with
     VFun (f) -> f v1 v2s c t m
-  | VContS (c', t') -> run_c2 c' v1 t' (MCons ((app_c, t), m))
+  | VContS (c', t') -> run_c2 c' v1 t' (MCons ((c, v2s, t), m))
   | VContC (c', t') -> run_c2 c' v1 (apnd t' (cons (fun v t m -> run_c2 app_c v t m) t)) m
   | _ -> failwith (to_string v0
                    ^ " is not a function; it can not be applied.")
