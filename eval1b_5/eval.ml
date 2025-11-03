5c491b0c882c7be29cd90e7121c5a81fd880d4d9
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-11-03 14:30:08 +0900

    eval1b_5: v2s' @ v2s

--- eval1b_5/eval.ml
+++ eval1b_5/eval.ml
@@ -45,9 +45,9 @@ let rec f e xs vs c t m =
                                ^ " are not numbers")
             end) t0 m0) t m
   | Fun (x, e) ->
-    c (VFun (fun v1 v2s v2s' c' t' m' ->
+    c (VFun (fun v1 v2s c' t' m' ->
               (* let app_c' = fun v0 t0 m0 -> app_s v0 v2s' c' t0 m0 in *)
-              f_t e (x :: xs) (v1 :: vs) v2s v2s' c' t' m')) t m
+              f_t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
   | App (e0, e2s) ->
     f_s e2s xs vs (fun v2s t2 m2 ->
       f e0 xs vs (fun v0 t0 m0 ->
@@ -69,8 +69,8 @@ let rec f e xs vs c t m =
   | Reset (e) -> f e xs vs idc TNil (MCons ((c, t), m))
 
 (* f_t : e -> string list -> v list -> v list -> c -> t -> m -> v *)
-and f_t e xs vs v2s v2s' c t m =
-  let c = fun v0 t0 m0 -> app_s v0 v2s' c t0 m0 in
+and f_t e xs vs v2s c t m =
+  (* let c = fun v0 t0 m0 -> app_s v0 v2s' c t0 m0 in *)
   let app_c = fun v t m -> app_s v v2s c t m in
   match e with
     Num (n) -> app_c (VNum (n)) t m
@@ -92,28 +92,28 @@ and f_t e xs vs v2s v2s' c t m =
                                ^ " are not numbers")
             end) t0 m0) t m
   | Fun (x, e) ->
-    app_c (VFun (fun v1 v2s v2s' c' t' m' ->
+    app_c (VFun (fun v1 v2s c' t' m' ->
               (* let app_c' = fun v0 t0 m0 -> app_s v0 v2s' c' t0 m0 in *)
-              f_t e (x :: xs) (v1 :: vs) v2s v2s' c' t' m')) t m
+              f_t e (x :: xs) (v1 :: vs) v2s c' t' m')) t m
   | App (e0, e2s) ->
     f_s e2s xs vs (fun v2s' t2 m2 ->
       f e0 xs vs (fun v0 t0 m0 ->
         app_st v0 v2s' v2s c t0 m0) t2 m2) t m (* app_s を app_st に *)
-  | Shift (x, e) -> f e (x :: xs) (VContS (c, t) :: vs) idc TNil m
-  | Control (x, e) -> f e (x :: xs) (VContC (c, t) :: vs) idc TNil m
+  | Shift (x, e) -> f e (x :: xs) (VContS (app_c, t) :: vs) idc TNil m
+  | Control (x, e) -> f e (x :: xs) (VContC (app_c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f e (x :: xs) (VContS (c, t) :: vs) c0 t0 m0
+          f e (x :: xs) (VContS (app_c, t) :: vs) c0 t0 m0
       | _ -> failwith "shift0 is used without enclosing reset"
     end
   | Control0 (x, e) ->
     begin match m with
         MCons ((c0, t0), m0) ->
-          f e (x :: xs) (VContC (c, t) :: vs) c0 t0 m0
+          f e (x :: xs) (VContC (app_c, t) :: vs) c0 t0 m0
       | _ -> failwith "control0 is used without enclosing reset"
     end
-  | Reset (e) -> f e xs vs idc TNil (MCons ((c, t), m))
+  | Reset (e) -> f e xs vs idc TNil (MCons ((app_c, t), m))
 
 (* f_s : e list -> string list -> v list -> c -> t -> m -> v list *)
 and f_s e2s xs vs c t m = match e2s with
@@ -135,14 +135,15 @@ and f_s e2s xs vs c t m = match e2s with
 and app v0 v1 v2s c t m =
   let app_c = fun v t m -> app_s v v2s c t m in
   match v0 with
-    VFun (f) -> f v1 v2s [] c t m
+    VFun (f) -> f v1 v2s c t m
   | VContS (c', t') -> c' v1 t' (MCons ((app_c, t), m))
   | VContC (c', t') -> c' v1 (apnd t' (cons app_c t)) m
   | _ -> failwith (to_string v0
                    ^ " is not a function; it can not be applied.")
 
 (* app_t : v -> v -> v list -> c -> t -> m -> v *)
-and app_t v0 v1 v2s' v2s c t m =
+and app_t v0 v1 v2s' v2s c t m = app v0 v1 (v2s' @ v2s) c t m
+  (*
   let app_c = fun v0 t0 m0 -> app_s v0 v2s c t0 m0 in
   match v0 with
     (* VFun (f) -> f v1 (v2s' @ v2s) c t m *)
@@ -153,16 +154,38 @@ and app_t v0 v1 v2s' v2s c t m =
   | VContC (c', t') -> c' v1 (apnd t' (cons app_c t)) m
   | _ -> failwith (to_string v0
                    ^ " is not a function; it can not be applied.")
+*)
 
 (* app_s : v -> v list -> c -> t -> m -> v *)
 and app_s v0 v2s c t m = match v2s with
     [] -> c v0 t m
   | v1 :: v2s -> app v0 v1 v2s c t m
 
+(*
+  (* v2s = [] *)
+  fun v t m -> app_s v v2s (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) t m
+= fun v t m -> app_s v [] (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) t m
+= fun v t m -> (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) v t m
+= fun v t m -> app_s v v2s' c t m
+= fun v t m -> app_s v ([] @ v2s') c t m
+
+  (* v2s = v1 :: v2s *)
+  fun v t m -> app_s v (v1 :: v2s) (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) t m
+= fun v t m -> app v v1 v2s (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) t m
+= fun v t m -> f v1 v2s [] (fun v0 t0 m0 -> app_s v0 v2s' c t0 m0) t m
+?
+= fun v t m -> f v1 (v2s @ v2s') [] c t m
+= fun v t m -> app v v1 (v2s @ v2s') c t m
+= fun v t m -> app_s v (v1 :: v2s @ v2s') c t m
+*)
+
 (* app_st : v -> v list -> v list -> c -> t -> m -> v *)
-and app_st v0 v2s' v2s c t m = match v2s' with
-    [] -> c v0 t m
+and app_st v0 v2s' v2s c t m = app_s v0 (v2s' @ v2s) c t m
+  (*
+  match v2s' with
+    [] -> app_s v0 v2s c t m
   | v1 :: v2s' -> app_t v0 v1 v2s' v2s c t m
+  *)
 
 (* f_init : e -> v *)
 let f_init expr = f expr [] [] idc TNil MNil
