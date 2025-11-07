d21aa228a2ccc07661b65ecaeaf46c6c1ca0c40a
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-11-07 21:46:06 +0900

    eval4a: copied from eval2a

---
+++ eval4a/value.ml
@@ -0,0 +1,37 @@
+open Syntax
+
+(* Definitional interpreter for λ-calculus with 4 delimited continuation operations : eval1 *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (v -> v list -> c -> t -> m -> v)
+       | VContS of c * t
+       | VContC of c * t
+
+and c = C0
+      | CApp1 of v list * c
+      | CApp2 of v list * c
+      | CAppT of v list * v list * c
+      | CAppS1 of e * string list * v list * c
+      | CAppS2 of e * string list * v list * c
+      | CAppST of e * string list * v list * v list * c
+      | COp0 of v * op * c
+      | COp1 of e * string list * op * v list * c
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * t) * m
+
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
