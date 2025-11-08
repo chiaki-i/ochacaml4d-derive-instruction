772625ef4ef2e573159536974c1150c4b6018910
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-11-08 17:27:05 +0900

    WIP: eval1b_3_2: 動くが、eval4b の実装につながらない形

--- eval1b_3_2/eval.ml
+++ eval1b_3_2/eval.ml
@@ -94,9 +94,9 @@ and f_t e xs vs v2s' c t m =
               f_t e (x :: xs) (v1 :: vs) v2s' c' t' m')) t m
   | App (e0, e2s) ->
     f_st e0 e2s xs vs v2s' c t m
-    (* f_s e2s xs vs (fun v2s t2 m2 ->
-        f e0 xs vs (fun v0 t0 m0 ->
-          app_s v0 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t2 m2) t m *)
+    (* f_st e2s xs vs v2s' (fun v2s t2 m2 -> (* 本当はこうなってほしい *)
+      f e0 xs vs (fun v0 t0 m0 ->
+        app_s v0 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t2 m2) t m *)
   | Shift (x, e) -> f e (x :: xs) (VContS (app_c, t) :: vs) idc TNil m
   | Control (x, e) -> f e (x :: xs) (VContC (app_c, t) :: vs) idc TNil m
   | Shift0 (x, e) ->
@@ -121,17 +121,57 @@ and f_s e2s xs vs c t m = match e2s with
       f e xs vs (fun v1 t1 m1 ->
         c (v1 :: v2s) t1 m1) t2 m2) t m
 
-(* f_st : e list -> string list -> v list -> v list -> c -> t -> m -> v *)
+(* f_st : e list -> string list -> v list -> v list -> c -> t -> m -> v list *)
+(* 本当はこうなってほしい *)
+(* and f_st e2s xs vs v2s' c t m = match e2s with
+    [] -> c v2s' t m
+  | e :: e2s ->
+    f_s e2s xs vs (fun v2s t2 m2 ->
+      f e xs vs (fun v1 t1 m1 ->
+        c (v1 :: v2s) t1 m1) t2 m2) t m *)
+
 and f_st e0 e2s xs vs v2s' c t m =
   let app_c = fun v2s t2 m2 ->
     f e0 xs vs (fun v0 t0 m0 ->
       app_s v0 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t2 m2 in
   match e2s with
-    [] -> app_c [] t m
+    [] ->
+    (* app_c [] t m *) (* eval1b_3_1 original *)
+    (* (fun v2s t2 m2 ->
+      f e0 xs vs (fun v0 t0 m0 ->
+        app_s v0 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t2 m2) [] t m *)
+    (* f e0 xs vs (fun v0 t0 m0 ->
+      app_s v0 [] (fun v t m -> app_s v v2s' c t m) t0 m0) t m *)
+    (* f e0 xs vs (fun v0 t0 m0 -> c v0 t0 m0) t m *)
+    f e0 xs vs c t m
   | e :: e2s ->
+    (* f_s e2s xs vs (fun v2s t2 m2 ->
+      f e xs vs (fun v1 t1 m1 ->
+        app_c (v1 :: v2s) t1 m1) t2 m2) t m *) (* eval1b_3_1 original *)
+    (* f_s e2s xs vs (fun v2s t2 m2 ->
+      f e xs vs (fun v1 t1 m1 ->
+        (fun v2s t2 m2 ->
+          f e0 xs vs (fun v0 t0 m0 ->
+            app_s v0 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t2 m2)
+        (v1 :: v2s) t1 m1) t2 m2) t m *) (* app_c を単に代入しただけ *)
+    (* f_s e2s xs vs (fun v2s t2 m2 ->
+      f e xs vs (fun v1 t1 m1 ->
+        f e0 xs vs (fun v0 t0 m0 ->
+          app_s v0 (v1 :: v2s) (fun v t m -> app_s v v2s' c t m) t0 m0) t1 m1)
+      t2 m2) t m *) (* (v1 :: v2s) t1 m1 を fun 文に代入する*)
+    (* f_s e2s xs vs (fun v2s t2 m2 ->
+      f e xs vs (fun v1 t1 m1 ->
+        f e0 xs vs (fun v0 t0 m0 ->
+          app v0 v1 v2s (fun v t m -> app_s v v2s' c t m) t0 m0) t1 m1)
+      t2 m2) t m *) (* fun 文を簡約して app_s の (v1 :: v2s) であるとわかる *)
     f_s e2s xs vs (fun v2s t2 m2 ->
       f e xs vs (fun v1 t1 m1 ->
-        app_c (v1 :: v2s) t1 m1) t2 m2) t m
+        f e0 xs vs (fun v0 t0 m0 ->
+          app v0 v1 (v2s @ v2s') c t0 m0) t1 m1)
+      t2 m2) t m
+    (* 恣意的な append… これが appterm を意味するが、
+    結局 v2s と v2s' を明示的に扱わないといけないので
+    VEmpty を用いた実装につながらない *)
 
 (* app : v -> v -> v list -> c -> t -> m -> v *)
 and app v0 v1 v2s' c t m =
