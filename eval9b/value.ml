f69fae97ab2c30cd463602a7edd24416bb5dc93f
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-11-15 22:42:58 +0900

    eval9b: copied from eval9a

---
+++ eval9b/value.ml
@@ -0,0 +1,52 @@
+open Syntax
+
+(* Definitional interpreter for λ-calculus with 4 delimited continuation operations : eval1 *)
+
+(* Value *)
+type v = VNum of int
+       | VFun of (c -> s -> t -> m -> v)
+       | VContS of c * s * t
+       | VContC of c * s * t
+       | VEmpty
+
+and c = C0
+      | CSeq of i * v list * c
+
+and i = ISeq of i * i
+      | IPushmark | IPass
+      | INum of int | IAccess of int | IOp of op
+      | IApply | IReturn
+      | ICur of i | IGrab of i
+      | IShift of i | IControl of i
+      | IShift0 of i | IControl0 of i
+      | IReset of i
+
+and s = v list
+
+and t = TNil | Trail of (v -> t -> m -> v)
+
+and m = MNil | MCons of (c * s * t) * m
+
+(* to_string : v -> string *)
+let rec to_string value = match value with
+    VNum (n) -> string_of_int n
+  | VFun (_) -> "<VFun>"
+  | VContS (_) -> "<VContS>"
+  | VContC (_) -> "<VContC>"
+  | VEmpty -> "<ε>"
+
+(* s_to_string : s -> string *)
+let rec s_to_string s =
+  "[" ^
+  begin match s with
+    [] -> ""
+  | first :: rest ->
+    to_string first ^
+    List.fold_left (fun str v -> str ^ "; " ^ to_string v) "" rest
+  end
+  ^ "]"
+
+(* Value.print : v -> unit *)
+let print exp =
+  let str = to_string exp in
+  print_string str
