d81307af6932e9a3f818f70f48e6d72eca3fba12
Chiaki Ishio <chiaki.ishio@gmail.com>
2025-11-15 16:48:19 +0900

    eval8a: combinators

--- eval8a/eval.ml
+++ eval8a/eval.ml
@@ -26,151 +26,47 @@ let apnd t0 t1 = match t0 with
     TNil -> t1
   | Trail (h) -> cons h t1
 
-(* run_c : c -> s -> t -> m -> v *)
-(* let rec run_c c s t m = match (c, s) with
-    (C0, v :: []) ->
-    begin match t with
-        TNil ->
-        begin match m with
-            MNil -> v
-          | MCons ((c, s, t), m) -> run_c c (v :: s) t m
-        end
-      | Trail (h) -> h v TNil m
-    end
-  | (COp1 (e, xs, op, vs, c), v :: s) ->
-    f e xs vs (COp0 (op, c)) (v :: s) t m
-  | (COp0 (op, c), v :: v0 :: s) ->
-    begin match (v, v0) with
-        (VNum (n0), VNum (n1)) ->
-        begin match op with
-            Plus -> run_c c (VNum (n0 + n1) :: s) t m
-          | Minus -> run_c c (VNum (n0 - n1) :: s) t m
-          | Times -> run_c c (VNum (n0 * n1) :: s) t m
-          | Divide ->
-            if n1 = 0 then failwith "Division by zero"
-            else run_c c (VNum (n0 / n1) :: s) t m
-        end
-      | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
-    end
-  | (CApp1 (c), v :: s) -> app_s v c s t m
-  | (CApp2 (c), v :: s) -> run_cs c (v :: s) t m *)
+(* (>>) : i -> i -> i *)
+let (>>) i0 i1 = fun vs c -> i0 vs (i1 vs c)
 
-(* run_cs : c -> s -> t -> m -> v *)
-(* and run_cs c s t m = match (c, s) with
-    (CAppS1 (e, xs, vs, c), s) -> f e xs vs (CApp1 (c)) s t m
-  | (CAppS2 (e, xs, vs, c), s) -> f e xs vs (CApp2 (c)) s t m *)
+(* num : int -> i *)
+let num n = fun vs c s t m -> c (VNum (n) :: s) t m
 
-(* f : definitional interpreter *)
-(* f : e -> string list -> v list -> c -> s -> t -> m -> v *)
-let rec f e xs vs c s t m =
-  match e with
-    Num (n) -> c (VNum (n) :: s) t m
-  | Var (x) -> c (List.nth vs (Env.off_set x xs) :: s) t m
-  | Op (e0, op, e1) ->
-    f e1 xs vs (fun (v :: s) t m ->
-      f e0 xs vs (fun (v :: v0 :: s) t m ->
-        begin match (v, v0) with
-            (VNum (n0), VNum (n1)) ->
-              begin match op with
-                  Plus -> c (VNum (n0 + n1) :: s) t m
-                | Minus -> c (VNum (n0 - n1) :: s) t m
-                | Times -> c (VNum (n0 * n1) :: s) t m
-                | Divide ->
-                  if n1 = 0 then failwith "Division by zero"
-                  else c (VNum (n0 / n1) :: s) t m
-              end
-          | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
-        end) (v :: s) t m) s t m
-  | Fun (x, e) ->
+(* access : int -> i *)
+let access n = fun vs c s t m -> c (List.nth vs n :: s) t m
+
+(* operation : op -> i *)
+let operation op = fun vs c (v :: v0 :: s) t m ->
+  begin match (v, v0) with
+      (VNum (n0), VNum (n1)) ->
+      begin match op with
+          Plus -> c (VNum (n0 + n1) :: s) t m
+        | Minus -> c (VNum (n0 - n1) :: s) t m
+        | Times -> c (VNum (n0 * n1) :: s) t m
+        | Divide ->
+          if n1 = 0 then failwith "Division by zero"
+          else c (VNum (n0 / n1) :: s) t m
+      end
+    | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
+  end
+
+(* cur : i -> i *)
+let cur i = fun vs c s t m ->
+  c ((VFun (fun c' (v1 :: s') t' m' ->
+    i (v1 :: vs) c' s' t' m')) :: s) t m
+
+(* grab: i -> i *)
+let grab i = fun vs c s t m ->
+  begin match s with
+    VEmpty :: s ->
     c ((VFun (fun c' (v1 :: s') t' m' ->
-      f_t e (x :: xs) (v1 :: vs) c' s' t' m')) :: s) t m
-  | App (e0, e2s) ->
-    f_s e2s xs vs (fun s2 t2 m2 ->
-      f e0 xs vs (fun (v0 :: s0) t0 m0 ->
-        app_s v0 c s0 t0 m0) s2 t2 m2) s t m
-  | Shift (x, e) -> f e (x :: xs) (VContS (c, s, t) :: vs) idc [] TNil m
-  | Control (x, e) -> f e (x :: xs) (VContC (c, s, t) :: vs) idc [] TNil m
-  | Shift0 (x, e) ->
-    begin match m with
-        MCons ((c0, s0, t0), m0) ->
-          f e (x :: xs) (VContS (c, s, t) :: vs) c0 s0 t0 m0
-      | _ -> failwith "shift0 is used without enclosing reset"
-    end
-  | Control0 (x, e) ->
-    begin match m with
-        MCons ((c0, s0, t0), m0) ->
-          f e (x :: xs) (VContC (c, s, t) :: vs) c0 s0 t0 m0
-      | _ -> failwith "control0 is used without enclosing reset"
-    end
-  | Reset (e) -> f e xs vs idc [] TNil (MCons ((c, s, t), m))
-
-(* f_t : e -> string list -> v list -> v list -> c -> s -> t -> m -> v *)
-and f_t e xs vs c s t m =
-  let app_c (v :: s) t m = app_s v c s t m in
-  match e with
-    Num (n) -> app_c (VNum (n) :: s) t m
-  | Var (x) -> app_c (List.nth vs (Env.off_set x xs) :: s) t m
-  | Op (e0, op, e1) ->
-    f e1 xs vs (fun (v :: s) t m ->
-      f e0 xs vs (fun (v :: v0 :: s) t m ->
-        begin match (v, v0) with
-            (VNum (n0), VNum (n1)) ->
-            begin match op with
-                Plus -> app_c (VNum (n0 + n1) :: s) t m
-              | Minus -> app_c (VNum (n0 - n1) :: s) t m
-              | Times -> app_c (VNum (n0 * n1) :: s) t m
-              | Divide ->
-                if n1 = 0 then failwith "Division by zero"
-                else app_c (VNum (n0 / n1) :: s) t m
-            end
-          | _ -> failwith (to_string v0 ^ " or " ^ to_string v ^ " are not numbers")
-        end) (v :: s) t m) s t m
-  | Fun (x, e) ->
-    begin match s with
-        VEmpty :: s -> c ((VFun (fun c' (v1 :: s') t' m' ->
-          f_t e (x :: xs) (v1 :: vs) c' s' t' m')) :: s) t m
-      | v1 :: s -> f_t e (x :: xs) (v1 :: vs) c s t m
-    end
-    (* app_c ((VFun (fun c' (v1 :: s') t' m' ->
-      f_t e (x :: xs) (v1 :: vs) c' s' t' m')) :: s) t m *)
-  | App (e0, e2s) ->
-    f_st e2s xs vs (fun s2 t2 m2 ->
-      f e0 xs vs (fun (v :: s1) t1 m1 ->
-        app_s v c s1 t1 m1) s2 t2 m2) s t m
-  | Shift (x, e) -> f e (x :: xs) (VContS (app_c, s, t) :: vs) idc [] TNil m
-  | Control (x, e) -> f e (x :: xs) (VContC (app_c, s, t) :: vs) idc [] TNil m
-  | Shift0 (x, e) ->
-    begin match m with
-        MCons ((c0, s0, t0), m0) ->
-          f e (x :: xs) (VContS (app_c, s, t) :: vs) c0 s0 t0 m0
-      | _ -> failwith "shift0 is used without enclosing reset"
-    end
-  | Control0 (x, e) ->
-    begin match m with
-        MCons ((c0, s0, t0), m0) ->
-          f e (x :: xs) (VContC (app_c, s, t) :: vs) c0 s0 t0 m0
-      | _ -> failwith "control0 is used without enclosing reset"
-    end
-  | Reset (e) -> f e xs vs idc [] TNil (MCons ((app_c, s, t), m))
-
-(* f_s : e list -> string list -> c -> s -> t -> m -> v list *)
-and f_s e2s xs vs c s t m = match e2s with
-    [] -> c (VEmpty :: s) t m
-  | e :: e2s ->
-    f_s e2s xs vs (fun s2 t2 m2 ->
-      f e xs vs (fun (v1 :: s1) t1 m1 ->
-        c (v1 :: s1) t1 m1) s2 t2 m2) s t m
-
-(* f_st : e list -> string list -> v list -> c -> s -> t -> m -> v list *)
-and f_st e2s xs vs c s t m = match e2s with
-    [] -> c s t m
-  | e :: e2s ->
-    f_st e2s xs vs (fun s2 t2 m2 ->
-      f e xs vs (fun (v1 :: s1) t1 m1 ->
-        c (v1 :: s1) t1 m1) s2 t2 m2) s t m
+      i (v1 :: vs) c' s' t' m')) :: s) t m
+  | v1 :: s -> i (v1 :: vs) c s t m
+  | _ -> failwith "grab: stack is empty"
+  end
 
 (* app : v -> v -> c -> s -> t -> m -> v *)
-and app v0 v1 c s t m =
+let rec app v0 v1 c s t m =
   let app_c (v :: s) t m = app_s v c s t m in
   match v0 with
     VFun (f) -> f c (v1 :: s) t m
@@ -186,6 +82,85 @@ and app_s v0 c s t m = match s with
     VEmpty :: s -> c (v0 :: s) t m
   | v1 :: s -> app v0 v1 c s t m
   | [] -> failwith "unexpected s"
+
+(* apply : i *)
+let apply = fun vs c (v :: s) t m ->
+  app_s v c s t m
+
+(* pushmark : i *)
+let pushmark = fun vs c s t m -> c (VEmpty :: s) t m
+
+(* pass : i *)
+let pass = fun vs c s t m -> c s t m
+
+(* return : i *)
+let return = fun vs c (v :: s) t m ->
+  app_s v c s t m
+
+(* shift : i -> i *)
+let shift i = fun vs c s t m ->
+  i (VContS (c, s, t) :: vs) idc [] TNil m
+
+(* control : i -> i *)
+let control i = fun vs c s t m ->
+  i (VContC (c, s, t) :: vs) idc [] TNil m
+
+(* shift0 : i -> i *)
+let shift0 i = fun vs c s t m -> match m with
+    MCons ((c0, s0, t0), m0) ->
+    i (VContS (c, s, t) :: vs) c0 s0 t0 m0
+  | _ -> failwith "shift0 is used without enclosing reset"
+
+(* control0 : i -> i *)
+let control0 i = fun vs c s t m -> match m with
+    MCons ((c0, s0, t0), m0) ->
+    i (VContC (c, s, t) :: vs) c0 s0 t0 m0
+  | _ -> failwith "control0 is used without enclosing reset"
+
+(* reset : i -> i *)
+let reset i = fun vs c s t m ->
+  i vs idc [] TNil (MCons ((c, s, t), m))
+
+(* f : definitional interpreter *)
+(* f : e -> string list -> v list -> c -> s -> t -> m -> v *)
+let rec f e xs = match e with
+    Num (n) -> num n
+  | Var (x) -> access (Env.off_set x xs)
+  | Op (e0, op, e1) ->
+    f e1 xs >> f e0 xs >> operation op
+  | Fun (x, e) -> cur (f_t e (x :: xs))
+  | App (e0, e2s) ->
+    f_s e2s xs >> f e0 xs >> apply
+  | Shift (x, e) -> shift (f e (x :: xs))
+  | Control (x, e) -> control (f e (x :: xs))
+  | Shift0 (x, e) -> shift0 (f e (x :: xs))
+  | Control0 (x, e) -> control0 (f e (x :: xs))
+  | Reset (e) -> reset (f e xs)
+
+(* f_t : e -> string list -> v list -> v list -> c -> s -> t -> m -> v *)
+and f_t e xs = match e with
+    Num (n) -> num n >> return
+  | Var (x) -> access (Env.off_set x xs) >> return
+  | Op (e0, op, e1) ->
+    f e1 xs >> f e0 xs >> operation op >> return
+  | Fun (x, e) -> grab (f_t e (x :: xs))
+  | App (e0, e2s) ->
+    f_st e2s xs >> f e0 xs >> apply
+  | Shift (x, e) -> shift (f e (x :: xs)) >> return
+  | Control (x, e) -> control (f e (x :: xs)) >> return
+  | Shift0 (x, e) -> shift0 (f e (x :: xs)) >> return
+  | Control0 (x, e) -> control0 (f e (x :: xs)) >> return
+  | Reset (e) -> reset (f e xs) >> return
+
+(* f_s : e list -> string list -> i *)
+and f_s e2s xs = match e2s with
+    [] -> pushmark
+  | e :: e2s -> f_s e2s xs >> f e xs
+
+(* f_st : e list -> string list -> i *)
+and f_st e2s xs = match e2s with
+    [] -> pass
+  | e :: e2s -> f_st e2s xs >> f e xs
 
 (* f_init : e -> v *)
 let f_init expr = f expr [] [] idc [] TNil MNil
